77adb46718188965e3bcf027e9e6c53a9282f37c
Владимир <mishchienko.2001@mail.ru>
2025-06-24 14:23:46 +0300

    Add database.py, creation engine and session

---
+++ src/database.py
@@ -0,0 +1,25 @@
+from typing import Annotated
+
+from sqlalchemy import NullPool, String
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+from src.config import settings
+
+
+# Проверка состояние (разработка или тестирование)
+if settings.MODE == "TEST":
+    DATABASE_URL = settings.TEST_DATABASE_URL
+    DATABASE_PARAMS = {"poolclass": NullPool}
+else:
+    DATABASE_URL = settings.DATABASE_URL
+    DATABASE_PARAMS = {}
+
+engine = create_async_engine(url=DATABASE_URL, **DATABASE_PARAMS)
+
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+str_256 = Annotated[str, 256]
+
+class Base(DeclarativeBase):
+    type_annotation_map = {str_256: String(256) }
