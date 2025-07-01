d6aa11aedc27b8a2706c465fa94c8a23532dfdac
Владимир <mishchienko.2001@mail.ru>
2025-07-01 19:47:57 +0300

    config read from env

--- fastapi-application/core/config.py
+++ fastapi-application/core/config.py
@@ -17,15 +17,20 @@ class ApiPrefix(BaseModel):
 class DataBaseConfig(BaseModel):
     """Подключение к базе данных"""
     url: PostgresDsn
-    echo: bool = False,
-    echo_pool: bool = False,
-    pool_size: int = 50,
-    max_overflow: int = 10,
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
 
 
 class Settings(BaseSettings):
     """Настройка приложения"""
-
+    model_config = SettingsConfigDict(
+        env_file=(".env.template", ".env"),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__",
+    )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
     db: DataBaseConfig
@@ -45,3 +50,4 @@ class Settings(BaseSettings):
 #     model_config = SettingsConfigDict(env_file=".env")
 #
 settings = Settings()
+
