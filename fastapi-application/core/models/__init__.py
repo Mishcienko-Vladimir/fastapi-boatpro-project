620dde7ec4e961544f48a23a403d6584319e6c8b
Владимир <mishchienko.2001@mail.ru>
2025-07-02 20:09:46 +0300

    Code correction file init

--- fastapi-application/core/models/__init__.py
+++ fastapi-application/core/models/__init__.py
@@ -1,4 +1,4 @@
-# Что бы не было двух уровней: from core.models.db_helper import db_helper -> from core.models import db_helper
+# Для удобного импорта: from core.models.db_helper import db_helper -> from core.models import db_helper
 
 __all__ = (
     "db_helper",
