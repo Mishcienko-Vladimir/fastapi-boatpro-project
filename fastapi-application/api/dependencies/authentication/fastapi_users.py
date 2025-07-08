dcafd460a3146b009c1d5fb710d842b217951d25
Владимир <mishchienko.2001@mail.ru>
2025-07-08 18:23:25 +0300

    Crate directory authentication

--- fastapi-application/api/dependencies/authentication/fastapi_users.py
+++ fastapi-application/api/dependencies/authentication/fastapi_users.py
@@ -1,7 +1,7 @@
 from fastapi_users import FastAPIUsers
 
-from api.dependencies.user_manager import get_user_manager
-from api.dependencies.backend import authentication_backend
+from api.dependencies.authentication.user_manager import get_user_manager
+from api.dependencies.authentication.backend import authentication_backend
 from core.models.user import User
 from core.types.user_id import UserIdType
 
