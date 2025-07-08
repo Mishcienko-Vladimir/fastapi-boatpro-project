754c36322c2b89fb09eeedbca6cf47b41be274db
Владимир <mishchienko.2001@mail.ru>
2025-07-08 18:42:25 +0300

    Fix users router

--- fastapi-application/api/api_v1/users.py
+++ fastapi-application/api/api_v1/users.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter
 
+from core.config import settings
 
-router = APIRouter(tags=["Users"])
+
+router = APIRouter(prefix=settings.api.v1.users, tags=["Users"])
+
 
 @router.get("/")
 async def get_users():
