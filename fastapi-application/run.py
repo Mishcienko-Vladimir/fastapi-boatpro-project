026165dcc4025d0de58f055f9e67e61ea84c88be
Владимир <mishchienko.2001@mail.ru>
2025-07-11 16:44:32 +0300

    Create run gunicorn for launch via docker

---
+++ fastapi-application/run.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# Для запуска приложения через docker, нужно разрешить командой: chmod +x ./run
+# Запускается командой: ./run
+
+from run_main import main
+
+if __name__ == "__main__":
+    main()
