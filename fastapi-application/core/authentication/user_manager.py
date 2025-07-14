d3c550f0569495e36e8346ebd3bde97de7229a04
Владимир <mishchienko.2001@mail.ru>
2025-07-14 18:14:35 +0300

    Add func webhooks-user in user-manager

--- fastapi-application/core/authentication/user_manager.py
+++ fastapi-application/core/authentication/user_manager.py
@@ -6,6 +6,7 @@ from fastapi_users import BaseUserManager, IntegerIDMixin
 from core.models.user import User
 from core.config import settings
 from core.types.user_id import UserIdType
+from utils.webhooks.user import send_new_user_notification
 
 if TYPE_CHECKING:
     from fastapi import Request
@@ -19,6 +20,8 @@ class UserManager(IntegerIDMixin, BaseUserManager[User, UserIdType]):
 
     async def on_after_register(self, user: User, request: Optional["Request"] = None):
         log.warning("User %r has registered.", user.id)
+        # отправка сообщения на сторонний сервис и вывод информации о том, что пользователь создан
+        # await send_new_user_notification(user)
 
     async def on_after_forgot_password(
         self, user: User, token: str, request: Optional["Request"] = None
