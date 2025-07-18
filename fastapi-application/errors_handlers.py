6cf90ca1a07813457a902ae96649d1199cda7936
Владимир <mishchienko.2001@mail.ru>
2025-07-18 23:55:30 +0300

    Creating a Database Error Handler

--- fastapi-application/errors_handlers.py
+++ fastapi-application/errors_handlers.py
@@ -5,6 +5,7 @@ import logging
 from fastapi import FastAPI, Request, status
 from fastapi.responses import ORJSONResponse
 from pydantic import ValidationError
+from sqlalchemy.exc import DatabaseError
 
 
 log = logging.getLogger(__name__)
@@ -24,5 +25,24 @@ def register_errors_handlers(app: FastAPI) -> None:
             content={
                 "message": "Unhandled error",
                 "error": exc.errors(),
+            },
+        )
+
+    @app.exception_handler(DatabaseError)
+    def handle_db_error(
+        request: Request,
+        exc: ValidationError,
+    ) -> ORJSONResponse:
+        """Обработчик ошибок базы данных"""
+
+        log.error(
+            "Unhandled database error",
+            exc_info=exc,
+        )
+        return ORJSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content={
+                "message": "An unexpected error has occurred. "
+                "Our admins are already working on it."
             },
         )
