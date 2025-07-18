aae15382560a55dd365a171e901c2fc578414e39
Владимир <mishchienko.2001@mail.ru>
2025-07-18 23:21:59 +0300

    transfer api creation to a separate file

--- fastapi-application/create_fastapi_app.py
+++ fastapi-application/create_fastapi_app.py
@@ -1,11 +1,26 @@
 # Создание FastAPI приложения и переопределение пути загрузки статики
 
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
 from fastapi.openapi.docs import (
     get_redoc_html,
     get_swagger_ui_html,
     get_swagger_ui_oauth2_redirect_html,
 )
+
+from api.webhooks import webhooks_router
+from core.models import db_helper
+
+
+# Для закрытия базы данных
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    # shutdown
+    await db_helper.dispose()
 
 
 def register_static_docs_routes(app: FastAPI):
@@ -30,3 +45,18 @@ def register_static_docs_routes(app: FastAPI):
             title=app.title + " - ReDoc",
             redoc_js_url="https://unpkg.com/redoc@next/bundles/redoc.standalone.js",
         )
+
+
+def create_app(
+    create_custom_static_urls: bool = False,
+) -> FastAPI:
+    app = FastAPI(
+        default_response_class=ORJSONResponse,
+        lifespan=lifespan,
+        docs_url=None if create_custom_static_urls else "/docs",
+        redoc_url=None if create_custom_static_urls else "/redoc",
+        webhooks=webhooks_router,
+    )
+    if create_custom_static_urls:
+        register_static_docs_routes(app)
+    return app
--- fastapi-application/main.py
+++ fastapi-application/main.py
@@ -1,15 +1,10 @@
-from contextlib import asynccontextmanager
-
 import uvicorn
 import logging
-from fastapi import FastAPI
-from fastapi.responses import ORJSONResponse
 
-from api.webhooks import webhooks_router
 from api import router as api_router
 from core.config import settings
-from core.models import db_helper
 from views import router as views_router
+from create_fastapi_app import create_app
 
 
 logging.basicConfig(
@@ -17,24 +12,16 @@ logging.basicConfig(
     format=settings.logging.log_format,
 )
 
+main_app = create_app(create_custom_static_urls=True)
 
-# Для закрытия базы данных
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # startup
-    yield
-    # shutdown
-    await db_helper.dispose()
-
-
-main_app = FastAPI(
-    default_response_class=ORJSONResponse, lifespan=lifespan, webhooks=webhooks_router
-)
 main_app.include_router(api_router)
 main_app.include_router(views_router)
 
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:main_app", host=settings.run.host, port=settings.run.port, reload=True
+        "main:main_app",
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=True,
     )
