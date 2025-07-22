961aa90207bacaa21912f9fc91563c062176a95a
Владимир <mishchienko.2001@mail.ru>
2025-07-22 16:47:44 +0300

    Add repr and str to motor

--- fastapi-application/core/models/motor.py
+++ fastapi-application/core/models/motor.py
@@ -15,3 +15,17 @@ class OutboardMotor(Base):
     image_id: Mapped[int]
     weight: Mapped[int]
     description: Mapped[str] = mapped_column(Text)
+
+    def __str__(self):
+        return (
+            f"{self.__class__.__name__}(id={self.id}, "
+            f"company_name={self.company_name!r}, "
+            f"engine_power={self.engine_power!r}, "
+            f"price={self.price!r}, "
+            f"image_id={self.image_id!r}, "
+            f"weight={self.weight!r}, "
+            f"description={self.description!r})"
+        )
+
+    def __repr__(self):
+        return str(self)
