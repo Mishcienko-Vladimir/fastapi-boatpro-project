3c3505dbc3d6cdd1e82ee3d168ec91e3dce4eb3a
Владимир <mishchienko.2001@mail.ru>
2025-07-22 23:59:57 +0300

    Creat trailer-base

--- fastapi-application/core/schemas/trailer.py
+++ fastapi-application/core/schemas/trailer.py
@@ -1,51 +1,47 @@
 from pydantic import BaseModel, ConfigDict, Field
 
+from core.schemas.base import BaseSchema
 
-class TrailerCreate(BaseModel):
-    """Схемы для создания трейлера"""
+
+class TrailerBase(BaseSchema):
+    """
+    Базовая схема для прицепа
+    """
 
     model_name: str = Field(
-        ...,
         min_length=1,
         max_length=255,
         description="Название модели прицепа",
     )
     full_mass: int = Field(
-        ...,
         gt=0,
         lt=32767,
         description="Полный вес прицепа в кг",
     )
     load_capacity: int = Field(
-        ...,
         gt=0,
         lt=32767,
         description="Грузоподъемность в кг",
     )
     trailer_length: int = Field(
-        ...,
         gt=0,
         lt=32767,
         description="Длина прицепа в мм",
     )
     max_ship_length: int = Field(
-        ...,
         gt=0,
         lt=32767,
         description="Максимальная длина перевозимого судна в мм",
     )
     description: str = Field(
-        default="",
         min_length=0,
         description="Описание прицепа",
     )
     price: int = Field(
-        ...,
         gt=0,
         description="Цена прицепа в рублях",
     )
     image_id: int = Field(
-        default=0,
         ge=0,
         description="ID изображения прицепа",
     )
