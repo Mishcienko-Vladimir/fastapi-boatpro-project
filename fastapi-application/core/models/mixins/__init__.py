8ea00477f660e62c800baa0ae6a3818a86fe286d
Владимир <mishchienko.2001@mail.ru>
2025-07-23 00:23:52 +0300

    Init updated-at-mixin

--- fastapi-application/core/models/mixins/__init__.py
+++ fastapi-application/core/models/mixins/__init__.py
@@ -1,7 +1,9 @@
 __all__ = (
     "IntIdPkMixin",
     "CreatedAtMixin",
+    "UpdatedAtMixin",
 )
 
 from core.models.mixins.int_id_pk_mixin import IntIdPkMixin
 from core.models.mixins.created_at_mixin import CreatedAtMixin
+from core.models.mixins.updated_at_mixin import UpdatedAtMixin
