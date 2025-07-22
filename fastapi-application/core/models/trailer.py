d1168613559618eefe9d8e88c35fc5578956718b
Владимир <mishchienko.2001@mail.ru>
2025-07-22 16:47:29 +0300

    Add repr and str to trailer

--- fastapi-application/core/models/trailer.py
+++ fastapi-application/core/models/trailer.py
@@ -17,3 +17,19 @@ class Trailer(Base):
     description: Mapped[str] = mapped_column(Text)
     price: Mapped[int]
     image_id: Mapped[int]
+
+    def __str__(self):
+        return (
+            f"{self.__class__.__name__}(id={self.id}, "
+            f"model_name={self.model_name!r}, "
+            f"full_mass={self.full_mass!r}, "
+            f"load_capacity={self.load_capacity!r}, "
+            f"overall_dimensions={self.overall_dimensions!r}, "
+            f"length_ship={self.length_ship!r}, "
+            f"description={self.description!r}, "
+            f"price={self.price!r}, "
+            f"image_id={self.image_id!r})"
+        )
+
+    def __repr__(self):
+        return str(self)
