1911addee8f5557b2dea174c782443a92e0e4e8b
Владимир <mishchienko.2001@mail.ru>
2025-07-22 18:00:51 +0300

    Init IntIdMixin + CreatedAtMixin

---
+++ fastapi-application/core/models/mixins/__init__.py
@@ -0,0 +1,7 @@
+__all__ = (
+    "IntIdPkMixin",
+    "CreatedAtMixin",
+)
+
+from core.models.mixins.int_id_pk_mixin import IntIdPkMixin
+from core.models.mixins.created_at_mixin import CreatedAtMixin
