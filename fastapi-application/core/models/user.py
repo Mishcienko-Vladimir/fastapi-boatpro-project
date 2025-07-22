cb87e8b4223f1476b3fac4c9ac78076b8184eecf
Владимир <mishchienko.2001@mail.ru>
2025-07-22 18:02:27 +0300

    fixing imports in model user

--- fastapi-application/core/models/user.py
+++ fastapi-application/core/models/user.py
@@ -1,13 +1,10 @@
 from typing import TYPE_CHECKING
 
-from fastapi_users_db_sqlalchemy import (
-    SQLAlchemyBaseUserTable,
-    SQLAlchemyUserDatabase
-)
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 
 from core.types.user_id import UserIdType
 from core.models.base import Base
-from core.models.mixins.int_id_pk import IntIdPkMixin
+from core.models.mixins import IntIdPkMixin
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
@@ -20,4 +17,3 @@ class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[UserIdType]):
     @classmethod
     def get_db(cls, session: "AsyncSession"):
         return SQLAlchemyUserDatabase(session, cls)
-
