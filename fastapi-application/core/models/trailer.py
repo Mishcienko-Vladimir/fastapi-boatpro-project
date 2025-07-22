798d322fb6ed6b886d833dd502cb5835d4f52c8f
Владимир <mishchienko.2001@mail.ru>
2025-07-22 20:15:12 +0300

    Refactoring trailer model

--- fastapi-application/core/models/trailer.py
+++ fastapi-application/core/models/trailer.py
@@ -1,22 +1,21 @@
-from sqlalchemy import Text
+from sqlalchemy import Text, SmallInteger, String
 from sqlalchemy.orm import Mapped, mapped_column
 
-from core.models.base import Base, str_256, intpk
+from core.models.base import Base
+from core.models.mixins import IntIdPkMixin, CreatedAtMixin
 
 
-class Trailer(Base):
+class Trailer(IntIdPkMixin, CreatedAtMixin, Base):
     """Таблица прицепов"""
-    __tablename__ = "trailers"
 
-    id: Mapped[intpk]
-    model_name: Mapped[str_256] = mapped_column(unique=True)
-    full_mass: Mapped[int]
-    load_capacity: Mapped[int]
-    overall_dimensions: Mapped[str_256]
-    length_ship: Mapped[int]
-    description: Mapped[str] = mapped_column(Text)
-    price: Mapped[int]
-    image_id: Mapped[int]
+    model_name: Mapped[str] = mapped_column(String(100), unique=True)
+    full_mass: Mapped[int] = mapped_column(SmallInteger, default=0)
+    load_capacity: Mapped[int] = mapped_column(SmallInteger, default=0)
+    trailer_length: Mapped[int] = mapped_column(SmallInteger, default=0)
+    max_length_transported_vessel: Mapped[int] = mapped_column(SmallInteger, default=0)
+    description: Mapped[str] = mapped_column(Text, default="", server_default="")
+    price: Mapped[int] = mapped_column(default=0)
+    image_id: Mapped[int] = mapped_column(default=0)
 
     def __str__(self):
         return (
@@ -24,8 +23,8 @@ class Trailer(Base):
             f"model_name={self.model_name!r}, "
             f"full_mass={self.full_mass!r}, "
             f"load_capacity={self.load_capacity!r}, "
-            f"overall_dimensions={self.overall_dimensions!r}, "
-            f"length_ship={self.length_ship!r}, "
+            f"trailer_length={self.trailer_length!r}, "
+            f"max_length_transported_vessel={self.max_length_transported_vessel!r}, "
             f"description={self.description!r}, "
             f"price={self.price!r}, "
             f"image_id={self.image_id!r})"
