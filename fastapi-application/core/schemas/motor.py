1f0020d66286ad36e9a8624345d184cb04f499f9
Владимир <mishchienko.2001@mail.ru>
2025-07-23 23:44:03 +0300

    Created base motor schema

---
+++ fastapi-application/core/schemas/motor.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import Field
+
+from core.schemas.base import BaseSchema
+
+
+class MotorBase(BaseSchema):
+    """
+    Базовая схема для лодочных моторов
+    """
+
+    company_name: str = Field(
+        min_length=1,
+        max_length=255,
+        description="Название производителя",
+    )
+    engine_power: int = Field(
+        gt=0,
+        lt=1000,
+        description="Мощность двигателя в л.с.",
+    )
+    price: int = Field(
+        gt=0,
+        description="Цена мотора в рублях",
+    )
+    weight: int = Field(
+        gt=0,
+        lt=1000,
+        description="Вес мотора в кг",
+    )
+    description: str = Field(
+        min_length=0,
+        description="Описание мотора",
+    )
+    image_id: int = Field(
+        ge=0,
+        description="ID изображения мотора",
+    )
