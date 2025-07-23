8be183feb4c7a5dc1a74dca73551f34051399af1
Владимир <mishchienko.2001@mail.ru>
2025-07-23 17:01:35 +0300

    Refactoring fields in the model motor

--- fastapi-application/core/models/motor.py
+++ fastapi-application/core/models/motor.py
@@ -1,20 +1,56 @@
-from sqlalchemy import Text
+from sqlalchemy import UniqueConstraint, Text, SmallInteger, String
 from sqlalchemy.orm import Mapped, mapped_column
 
-from core.models.base import Base, str_256, intpk
+from core.models.base import Base
+from core.models.mixins import IntIdPkMixin, CreatedAtMixin, UpdatedAtMixin
 
 
-class OutboardMotor(Base):
-    """Таблица подвесных лодочных моторов"""
-    __tablename__ = "outboard_motors"
+class OutboardMotor(
+    IntIdPkMixin,
+    CreatedAtMixin,
+    UpdatedAtMixin,
+    Base,
+):
+    """
+    Таблица подвесных лодочных моторов.
 
-    id: Mapped[intpk]
-    company_name: Mapped[str_256]
-    engine_power: Mapped[int]
-    price: Mapped[int]
-    image_id: Mapped[int]
-    weight: Mapped[int]
-    description: Mapped[str] = mapped_column(Text)
+    Уникальность модели обеспечивается по комбинации:
+    - company_name
+    - engine_power
+    """
+
+    __table_args__ = (
+        UniqueConstraint("company_name", "engine_power", name="uq_company_engine"),
+    )
+
+    company_name: Mapped[str] = mapped_column(
+        String(100),
+        comment="Название производителя",
+    )
+    engine_power: Mapped[int] = mapped_column(
+        SmallInteger,
+        default=0,
+        comment="Мощность двигателя в л.с.",
+    )
+    price: Mapped[int] = mapped_column(
+        default=0,
+        comment="Цена мотора в рублях",
+    )
+    weight: Mapped[int] = mapped_column(
+        SmallInteger,
+        default=0,
+        comment="Вес мотора в кг",
+    )
+    description: Mapped[str] = mapped_column(
+        Text,
+        default="",
+        server_default="",
+        comment="Описание мотора",
+    )
+    image_id: Mapped[int] = mapped_column(
+        default=0,
+        comment="ID изображения мотора",
+    )
 
     def __str__(self):
         return (
@@ -22,9 +58,9 @@ class OutboardMotor(Base):
             f"company_name={self.company_name!r}, "
             f"engine_power={self.engine_power!r}, "
             f"price={self.price!r}, "
-            f"image_id={self.image_id!r}, "
             f"weight={self.weight!r}, "
-            f"description={self.description!r})"
+            f"description={self.description!r}, "
+            f"image_id={self.image_id!r})"
         )
 
     def __repr__(self):
