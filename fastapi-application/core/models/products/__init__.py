3690c5738319340742c54e7d95d6261ed09a7495
Владимир <mishchienko.2001@mail.ru>
2025-07-24 17:56:18 +0300

    Init model boat, out-board-motor, trailer, product-type and product-base

---
+++ fastapi-application/core/models/products/__init__.py
@@ -0,0 +1,13 @@
+__all__ = (
+    "ProductType",
+    "ProductBase",
+    "Boat",
+    "OutboardMotor",
+    "Trailer",
+)
+
+from .product_type import ProductType
+from .product_base import ProductBase
+from .boat import Boat
+from .motor import OutboardMotor
+from .trailer import Trailer
