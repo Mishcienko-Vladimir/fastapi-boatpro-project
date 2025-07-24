ba933d6c6576c4479f0ad2d6e45544a7aa36ac05
Владимир <mishchienko.2001@mail.ru>
2025-07-24 17:53:36 +0300

    Create product-base for model products

---
+++ fastapi-application/core/models/products/product_base.py
@@ -0,0 +1,46 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import Text, String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from core.models.base import Base
+from core.models.mixins import IntIdPkMixin, CreatedAtMixin, UpdatedAtMixin
+
+if TYPE_CHECKING:
+    from core.models.products import ProductType
+
+
+class ProductBase(
+    IntIdPkMixin,
+    CreatedAtMixin,
+    UpdatedAtMixin,
+    Base,
+):
+    """
+    Базовая модель для продуктов
+    """
+
+    __abstract__ = True  # Абстрактная модель, не создает таблицу
+
+    type_id: Mapped[int] = mapped_column(
+        ForeignKey("product_types.id"),
+    )
+    price: Mapped[int] = mapped_column(
+        comment="Цена в рублях",
+    )
+    company_name: Mapped[str] = mapped_column(
+        String(100),
+        comment="Название производителя",
+    )
+    description: Mapped[str] = mapped_column(
+        Text,
+        comment="Описание",
+    )
+    image_id: Mapped[list[int]] = mapped_column(
+        comment="ID изображения",
+    )
+    is_active: Mapped[bool] = mapped_column(
+        default=True,
+    )
+
+    type: Mapped["ProductType"] = relationship(back_populates="products")
