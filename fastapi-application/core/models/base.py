c97010b45b13629656fcf9d96cd25afae7c9148e
Владимир <mishchienko.2001@mail.ru>
2025-07-25 15:06:45 +0300

    Refactoring base

--- fastapi-application/core/models/base.py
+++ fastapi-application/core/models/base.py
@@ -1,18 +1,13 @@
-from typing import Annotated
-
-from sqlalchemy import String, MetaData
-from sqlalchemy.orm import DeclarativeBase, mapped_column, declared_attr
+from sqlalchemy import MetaData
+from sqlalchemy.orm import DeclarativeBase, declared_attr
 
 from core.config import settings
 from utils import camel_case_to_snake_case
 
 
-intpk = Annotated[int, mapped_column(primary_key=True, autoincrement=True)]
-str_256 = Annotated[str, 256]
-
-
 class Base(DeclarativeBase):
     """Базовая модель для всех таблиц"""
+
     __abstract__ = True
 
     # Для имен миграций
@@ -24,5 +19,3 @@ class Base(DeclarativeBase):
     def __tablename__(cls) -> str:
         """Конвертация имени таблицы"""
         return f"{camel_case_to_snake_case(cls.__name__)}s"
-
-    type_annotation_map = {str_256: String(256)}
