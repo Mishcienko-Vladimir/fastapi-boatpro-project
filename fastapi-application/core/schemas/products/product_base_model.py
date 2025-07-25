cda71d562d81e19ebfa474d3dad5616136290791
Владимир <mishchienko.2001@mail.ru>
2025-07-25 19:58:26 +0300

    Add columns product-base-model

--- fastapi-application/core/schemas/products/product_base_model.py
+++ fastapi-application/core/schemas/products/product_base_model.py
@@ -1,5 +1,34 @@
+from pydantic import Field
+
 from core.schemas.base_model import BaseSchemaModel
 
 
 class ProductBaseModel(BaseSchemaModel):
-    pass
+    """
+    Базовая модель для товаров.
+    """
+
+    model_name: str = Field(
+        min_length=1,
+        max_length=255,
+        description="Название модели",
+    )
+    price: int = Field(
+        gt=0,
+        description="Цена в рублях",
+    )
+    company_name: str = Field(
+        min_length=1,
+        max_length=100,
+        description="Название производителя",
+    )
+    description: str = Field(
+        min_length=0,
+        description="Описание",
+    )
+    image_id: list[int] = Field(
+        description="ID изображения",
+    )
+    is_active: bool = Field(
+        description="Наличие товара",
+    )
