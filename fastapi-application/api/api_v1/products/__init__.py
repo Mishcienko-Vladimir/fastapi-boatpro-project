f0688eadc1af1c768762d42cefc988b00837c696
Владимир <mishchienko.2001@mail.ru>
2025-07-27 21:48:25 +0300

    Init boats, trailers, motors and product-types routers

--- fastapi-application/api/api_v1/products/__init__.py
+++ fastapi-application/api/api_v1/products/__init__.py
@@ -2,5 +2,15 @@ from fastapi import APIRouter
 
 from core.config import settings
 
+from .boats import router as boats_router
+from .trailers import router as trailers_router
+from .motors import router as motors_router
+from .product_types import router as product_types_router
 
-router = APIRouter(prefix=settings.api.v1.prefix.products)
+
+router = APIRouter(prefix=settings.api.v1.products)
+
+router.include_router(boats_router)
+router.include_router(trailers_router)
+router.include_router(motors_router)
+router.include_router(product_types_router)
