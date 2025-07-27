b5b16a60e36163b15645eda7b0be0255a4bb9367
Владимир <mishchienko.2001@mail.ru>
2025-07-27 16:44:31 +0300

    Init products-router

--- fastapi-application/api/api_v1/__init__.py
+++ fastapi-application/api/api_v1/__init__.py
@@ -5,6 +5,7 @@ from core.config import settings
 from .users import router as users_router
 from .auth import router as auth_router
 from .messages import router as messages_router
+from .products import router as products_router
 
 
 http_bearer = HTTPBearer(auto_error=False)
@@ -14,3 +15,4 @@ router = APIRouter(prefix=settings.api.v1.prefix, dependencies=[Depends(http_bea
 router.include_router(users_router)
 router.include_router(auth_router)
 router.include_router(messages_router)
+router.include_router(products_router)
