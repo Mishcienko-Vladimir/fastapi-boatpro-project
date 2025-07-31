ccd8d55747ef05ed2464c2a610649cfe6ce564ec
Владимир <mishchienko.2001@mail.ru>
2025-07-31 23:05:05 +0300

    Add endpoint get-trailer-by-name

--- fastapi-application/api/api_v1/routers/products/trailers.py
+++ fastapi-application/api/api_v1/routers/products/trailers.py
@@ -1,11 +1,26 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 from core.config import settings
+from core.schemas.products import TrailerRead
+
+from api.api_v1.services.products.trailers import TrailerService
 
 
 router = APIRouter(prefix=settings.api.v1.trailers, tags=["Прицепы"])
 
 
-@router.get("/")
-def get_trailers():
-    return {"message": "Hello, World!"}
+@router.get("/{name}", response_model=TrailerRead)
+async def get_trailer_by_name(
+    name: str,
+    service: TrailerService = Depends(),
+):
+    return await service.get_trailer_by_name(name)
+
+
+# @router.post("/", response_model=TrailerCreate)
+# async def create_trailer(
+#     trailer_data: TrailerCreate,
+#     trailers_service: TrailerService = Depends(),
+# ):
+#     new_trailer = await trailers_service.create_trailer(trailer_data)
+#     return new_trailer
