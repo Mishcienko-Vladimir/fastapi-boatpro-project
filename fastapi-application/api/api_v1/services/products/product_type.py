abee8c0bae6a66c3c9bbaacba7c4c1ffc8ebc218
Владимир <mishchienko.2001@mail.ru>
2025-08-03 22:47:14 +0300

    Creating service for product-type and add func create-product-type

---
+++ fastapi-application/api/api_v1/services/products/product_type.py
@@ -0,0 +1,25 @@
+from fastapi import HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models.products import ProductType
+from core.schemas.products import ProductTypeCreate, ProductTypeRead
+from core.repositories.products.product_manager_crud import ProductManagerCrud
+
+
+class ProductTypeService:
+
+    def __init__(self, session: AsyncSession):
+        self.repo = ProductManagerCrud(session, ProductType)
+
+    async def create_product_type(
+        self, product_data: ProductTypeCreate
+    ) -> ProductTypeRead:
+        """
+        Создание нового прицепа
+        """
+        # Проверка на существование типа товара
+        if await self.repo.product_type_exists_by_name(product_data.name_product_type):
+            raise HTTPException(status_code=400, detail=f"Product type already exists")
+
+        new_product_type = await self.repo.create_product(product_data)
+        return ProductTypeRead.model_validate(new_product_type)
