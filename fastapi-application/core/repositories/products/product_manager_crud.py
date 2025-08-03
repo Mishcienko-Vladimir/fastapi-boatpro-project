13fc29fac79fcf7b3c1ac273feec834ce29870f9
Владимир <mishchienko.2001@mail.ru>
2025-08-03 22:44:51 +0300

    Add examination product type exists by name

--- fastapi-application/core/repositories/products/product_manager_crud.py
+++ fastapi-application/core/repositories/products/product_manager_crud.py
@@ -1,11 +1,9 @@
 from typing import TYPE_CHECKING, Annotated
 from fastapi import Depends
 from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import db_helper
-
-if TYPE_CHECKING:
-    from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class ProductManagerCrud:
@@ -15,36 +13,39 @@ class ProductManagerCrud:
 
     def __init__(
         self,
-        session: Annotated[
-            AsyncSession,
-            Depends(db_helper.session_getter),
-        ],
+        session: AsyncSession,
         product_db,
-        product_create,
-        product_update,
     ):
         self.session = session
         self.product_db = product_db
-        self.product_create = product_create
-        self.product_update = product_update
 
-    async def create_product(self):
+    async def create_product(self, product_data):
         """
         Создает новый товар.
         """
 
-        new_product = self.product_db(**self.product_create.model_dump())
+        new_product = self.product_db(**product_data.model_dump())
         self.session.add(new_product)
         await self.session.commit()
         return new_product
+
+    async def product_type_exists_by_name(self, name) -> bool:
+        """
+        Проверяет, существует ли тип товара с заданным именем.
+        """
+
+        stmt = select(self.product_db).filter_by(name_product_type=name)
+        result = await self.session.execute(stmt)
+        return result.scalars().first()
 
     async def get_product_by_name(self, model_name: str):
         """
         Найдет товар по model_name.
         """
-        stmt = select(self.product_db).where(self.product_db.model_name == model_name)  # type: ignore
+
+        stmt = select(self.product_db).filter_by(model_name=model_name)
         result = await self.session.execute(stmt)
-        return result.scalar_one_or_none()
+        return result.scalars().first()
 
     async def get_product_by_id(self, product_id: int):
         """
@@ -53,7 +54,7 @@ class ProductManagerCrud:
 
         return await self.session.get(self.product_db, product_id)
 
-    async def get_all_product(self):
+    async def get_all_products(self):
         """
         Получает все товары.
         """
@@ -62,14 +63,14 @@ class ProductManagerCrud:
         result = await self.session.scalars(stmt)
         return result.all()
 
-    async def update_product(self, product_id: int):
+    async def update_product(self, product_update_schema, product_id: int):
         """
         Обновляет товар по id.
         """
 
         product = await self.get_product_by_id(product_id)
         if product:
-            for name, value in self.product_update.model_dump(
+            for name, value in product_update_schema.model_dump(
                 exclude_unset=True
             ).items():
                 setattr(product, name, value)
@@ -77,14 +78,14 @@ class ProductManagerCrud:
             return product
         return None
 
-    async def delete_product(self, product_id: int) -> bool:
-        """
-        Удаляет товар по id.
-        """
-
-        product = await self.get_product_by_id(product_id)
-        if product:
-            await self.session.delete(product)
-            await self.session.commit()
-            return True
-        return False
+    # async def delete_product(self, product_id: int) -> bool:
+    #     """
+    #     Удаляет товар по id.
+    #     """
+    #
+    #     product = await self.get_product_by_id(product_id)
+    #     if product:
+    #         await self.session.delete(product)
+    #         await self.session.commit()
+    #         return True
+    #     return False
