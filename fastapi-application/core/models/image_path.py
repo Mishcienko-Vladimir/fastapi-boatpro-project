eb9e36584b31bd16aa944a7dbec8cf8be85df021
Владимир <mishchienko.2001@mail.ru>
2025-08-05 23:35:39 +0300

    Creat image-path model

--- fastapi-application/core/models/image_path.py
+++ fastapi-application/core/models/image_path.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Text, String, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
 
 from core.models.base import Base
 from core.models.mixins import IntIdPkMixin
@@ -13,7 +13,7 @@ class ImagePath(IntIdPkMixin, Base):
     path: Mapped[str] = mapped_column(String(255), comment="Путь к изображению")
 
     def __str__(self):
-        return f"{self.__class__.__name__}" f"(id={self.id!r}, " f"path='{self.path}')"
+        return f"{self.__class__.__name__} (id={self.id!r}, path={self.path!r})"
 
     def __repr__(self):
         return str(self)
