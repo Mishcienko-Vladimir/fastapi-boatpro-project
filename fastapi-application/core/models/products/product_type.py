a56491c8f44c2ebc8b970e041ae5d2652e9645bc
Владимир <mishchienko.2001@mail.ru>
2025-08-06 00:21:04 +0300

    Refactoring relationship for product-type

--- fastapi-application/core/models/products/product_type.py
+++ fastapi-application/core/models/products/product_type.py
@@ -7,7 +7,7 @@ from core.models.base import Base
 from core.models.mixins import IntIdPkMixin
 
 if TYPE_CHECKING:
-    from core.models.products.product_base import ProductBase
+    from core.models.products import Trailer, Boat, OutboardMotor
 
 
 class ProductType(IntIdPkMixin, Base):
@@ -19,13 +19,15 @@ class ProductType(IntIdPkMixin, Base):
 
     name_product_type: Mapped[str] = mapped_column(String(50), unique=True, index=True)
 
-    products: Mapped[list["ProductBase"]] = relationship(back_populates="type")
+    trailers: Mapped[list["Trailer"]] = relationship(back_populates="type")
+    boats: Mapped[list["Boat"]] = relationship(back_populates="type")
+    Outboard_motors: Mapped[list["OutboardMotor"]] = relationship(back_populates="type")
 
     def __str__(self):
         return (
             f"{self.__class__.__name__}"
             f"(id={self.id!r}, "
-            f"name_product_type='{self.name_product_type}')"
+            f"name_product_type='{self.name_product_type!r}')"
         )
 
     def __repr__(self):
