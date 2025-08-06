633003610ccb3d7f886d600a1763d08e3a4fdd1b
Владимир <mishchienko.2001@mail.ru>
2025-08-06 22:01:25 +0300

    Fix image path model

--- fastapi-application/core/models/image_path.py
+++ fastapi-application/core/models/image_path.py
@@ -13,9 +13,11 @@ class ImagePath(IntIdPkMixin, Base):
 
     path: Mapped[str] = mapped_column(String(255), comment="Путь к изображению")
 
-    trailer_id: Mapped[int] = mapped_column(ForeignKey("trailers.id"))
-    boat_id: Mapped[int] = mapped_column(ForeignKey("boats.id"))
-    outboard_motor_id: Mapped[int] = mapped_column(ForeignKey("outboard_motors.id"))
+    trailer_id: Mapped[int | None] = mapped_column(ForeignKey("trailers.id"))
+    boat_id: Mapped[int | None] = mapped_column(ForeignKey("boats.id"))
+    outboard_motor_id: Mapped[int | None] = mapped_column(
+        ForeignKey("outboard_motors.id")
+    )
 
     def __str__(self):
         return f"{self.__class__.__name__} (id={self.id!r}, path={self.path!r})"
