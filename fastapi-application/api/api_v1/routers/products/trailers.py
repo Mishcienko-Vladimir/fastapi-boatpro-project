d8043da66282028b90ca1af3fe9b0702f64bd48b
Владимир <mishchienko.2001@mail.ru>
2025-08-06 22:43:59 +0300

    Commit router trailers

--- fastapi-application/api/api_v1/routers/products/trailers.py
+++ fastapi-application/api/api_v1/routers/products/trailers.py
@@ -3,18 +3,18 @@ from fastapi import APIRouter, Depends
 from core.config import settings
 from core.schemas.products import TrailerRead
 
-from api.api_v1.services.products.trailers import TrailerService
+# from api.api_v1.services.products.trailers import TrailerService
 
 
 router = APIRouter(prefix=settings.api.v1.trailers, tags=["Прицепы"])
 
 
-@router.get("/{name}", response_model=TrailerRead)
-async def get_trailer_by_name(
-    name: str,
-    service: TrailerService = Depends(),
-):
-    return await service.get_trailer_by_name(name)
+# @router.get("/{name}", response_model=TrailerRead)
+# async def get_trailer_by_name(
+#     name: str,
+#     service: TrailerService = Depends(),
+# ):
+#     return await service.get_trailer_by_name(name)
 
 
 # @router.post("/", response_model=TrailerCreate)
