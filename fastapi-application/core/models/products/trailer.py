e9b1d564dfa4d625a45c1d2464fa965546c1f350
Владимир <mishchienko.2001@mail.ru>
2025-08-07 17:21:06 +0300

    Refactoring model trailer

--- fastapi-application/core/models/products/trailer.py
+++ fastapi-application/core/models/products/trailer.py
@@ -1,16 +1,23 @@
-from sqlalchemy import SmallInteger
+from sqlalchemy import SmallInteger, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
-from core.models.products import ProductBase
+from core.models.products.product_base import Product
 
 
-class Trailer(ProductBase):
+class Trailer(Product):
     """
     Таблица прицепов.
 
-    Уникальность обеспечивается по полю model_name.
+    Уникальность обеспечивается по полю name и id.
     """
 
+    __mapper_args__ = {"polymorphic_identity": "trailer"}
+
+    id: Mapped[int] = mapped_column(
+        ForeignKey("products.id"),
+        primary_key=True,
+        doc="Для полиморфной связи с таблицей products",
+    )
     full_mass: Mapped[int] = mapped_column(
         SmallInteger,
         comment="Общая масса прицепа в кг",
@@ -32,7 +39,7 @@ class Trailer(ProductBase):
         return (
             f"{self.__class__.__name__}"
             f"(id={self.id}, "
-            f"model_name={self.model_name!r}, "
+            f"name={self.name!r}, "
             f"price={self.price!r}, "
             f"company_name={self.company_name!r}, "
             f"description={self.description!r}, "
@@ -42,8 +49,8 @@ class Trailer(ProductBase):
             f"load_capacity={self.load_capacity!r}, "
             f"trailer_length={self.trailer_length!r}, "
             f"max_ship_length={self.max_ship_length!r}, "
-            f"type_id={self.type_id!r}, "
-            f"type={self.type!r}, "
+            f"category_id={self.category_id!r}, "
+            f"type_product={self.type_product!r}, "
             f"created_at={self.created_at!r}, "
             f"updated_at={self.updated_at!r})"
         )
