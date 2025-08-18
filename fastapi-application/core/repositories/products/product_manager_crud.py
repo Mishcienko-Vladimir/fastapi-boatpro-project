52782a7a7e80c982c5069ef7ed0e354eb098105c
Владимир <mishchienko.2001@mail.ru>
2025-08-18 21:50:30 +0300

    Refactoring code in product-manager-crud

--- fastapi-application/core/repositories/products/product_manager_crud.py
+++ fastapi-application/core/repositories/products/product_manager_crud.py
@@ -1,20 +1,14 @@
-import aiofiles
-from aiofiles import os
-
 from sqlalchemy import select
 from sqlalchemy.orm import joinedload
 from sqlalchemy.ext.asyncio import AsyncSession
-from uuid import uuid4
-
-
-from core.config import settings
-from core.models.products import ImagePath
-from core.repositories.products.image_manager import ImageManager
 
 
 class ProductManagerCrud:
     """
     Помощник для работы с товарами.
+
+    :session: - сессия для работы с БД.
+    :product_db: - модель БД (Boat, Trailer и т.д.).
     """
 
     def __init__(
@@ -23,25 +17,14 @@ class ProductManagerCrud:
         product_db,
     ):
         self.session = session
-        self.product_db = product_db  # Тип модели SQLAlchemy (Boat, Trailer и т.д.)
-
-    async def create_product_with_images(self, product_data, images):
-        """
-        Создает новый товар с изображением.
-        """
-
-        product = self.product_db(**product_data.model_dump())
-        self.session.add(product)
-
-        _service = ImageManager(self.session)
-        new_product = await _service.add_image_to_db(product, images)
-
-        await self.session.commit()
-        return new_product
+        self.product_db = product_db
 
     async def get_product_by_name(self, name: str):
         """
-        Найдет товар по name.
+        Получает товар по name.
+
+        :name: - имя товара.
+        :return: - экземпляр модели товара или None.
         """
 
         stmt = (
@@ -58,6 +41,9 @@ class ProductManagerCrud:
     async def get_product_by_id(self, product_id: int):
         """
         Получает товар по id.
+
+        :param product_id: - id товара.
+        :return: - экземпляр модели товара или None.
         """
 
         stmt = (
@@ -74,6 +60,8 @@ class ProductManagerCrud:
     async def get_all_products(self):
         """
         Получает все товары.
+
+        :return: - список всех товаров или None.
         """
 
         stmt = select(self.product_db).options(
@@ -83,115 +71,45 @@ class ProductManagerCrud:
         result = await self.session.execute(stmt)
         return result.scalars().unique().all()
 
-    async def update_product_data_by_id(
-        self,
-        product_id: int,
-        product_update_schema,
-    ):
+    async def create_product(self, product_data):
         """
-        Обновляет товар по id, без обработки изображений.
+        Создает новый товар.
+
+        :product_data: - данные для создания товара.
+        :return: - экземпляр модели товара.
         """
 
-        product = await self.get_product_by_id(product_id)
+        product = self.product_db(**product_data.model_dump())
+        self.session.add(product)
 
-        if product:
-            for name, value in product_update_schema.model_dump(
-                exclude_unset=True
-            ).items():
-                setattr(product, name, value)
-            await self.session.commit()
-            return product
-        return None
-
-    async def update_product_images_by_id(
-        self,
-        product_id: int,
-        remove_images_list,
-        add_images,
-    ):
-        product = await self.get_product_by_id(product_id)
-
-        if not product:
-            return None
-
-        if remove_images_list:
-            # Удаление изображений с диска
-            for image in remove_images_list:
-                # Удаление записи из ImagePath
-                stmt = select(ImagePath).filter_by(id=image)
-                result = await self.session.execute(stmt)
-                image_record = result.scalars().first()
-
-                # Получаем путь к изображению
-                file_path = f"{settings.image_upload_dir.base_dir}{image_record.path}"
-                product.images.clear(image_record.path)
-
-                if image_record:
-                    await self.session.delete(image_record)
-
-                try:
-                    # Удаляем файл с диска
-                    await aiofiles.os.remove(file_path)
-                except FileNotFoundError:
-                    # Файл не найден — можно логировать ошибку
-                    print(f"Файл {file_path} не найден.")
-
-        # Если есть новые изображения, сохраняем их
-        if add_images:
-            for image in add_images:
-                file_path = f"{settings.image_upload_dir.path}\\{uuid4().hex}.jpg"
-
-                # Сохранение изображений в папку .../MFBoats/fastapi-application/static/images
-                async with aiofiles.open(file_path, "wb") as file:
-                    await file.write(image.file.read())
-
-                # Сокращаем путь до /static/images/...
-                shortened_path = file_path.partition("fastapi-application")[2]
-
-                # Создаем запись в таблицу ImagePath
-                image_path = ImagePath(path=shortened_path)
-                self.session.add(image_path)
-
-                # Добавляем изображение к прицепу
-                product.images.append(image_path)
         await self.session.commit()
         return product
 
-    async def delete_product_by_id(self, product_id: int) -> bool:
+    async def update_product_data(self, product, product_data):
         """
-        Удаляет товар по id.
+        Обновляет товар, без обработки изображений.
+
+        :param product: - экземпляр модели товара для обновления.
+        :param product_data: - данные для обновления.
+        :return: - обновленный экземпляр модели товара.
         """
 
-        product = await self.get_product_by_id(product_id)
+        for name, value in product_data.model_dump(exclude_unset=True).items():
+            setattr(product, name, value)
+        await self.session.commit()
+        return product
 
-        if product:
-            # Получаем список связанных изображений
-            related_images = list(product.images)
+    async def delete_product(self, product) -> bool:
+        """
+        Удаляет товар.
 
-            # Удаление изображений с диска
-            for image in related_images:
-                # Получаем путь к изображению
-                file_path = f"{settings.image_upload_dir.base_dir}{image.path}"
+        :param product: - экземпляр модели товара для удаления.
+        :return: - удаление прошло успешно True.
+        """
 
-                # Удаление записи из ImagePath
-                stmt = select(ImagePath).filter_by(id=image.id)
-                result = await self.session.execute(stmt)
-                image_record = result.scalars().first()
-
-                if image_record:
-                    await self.session.delete(image_record)
-
-                try:
-                    # Удаляем файл с диска
-                    await aiofiles.os.remove(file_path)
-                except FileNotFoundError:
-                    # Файл не найден — можно логировать ошибку
-                    print(f"Файл {file_path} не найден.")
-
-            await self.session.delete(product)
-            await self.session.commit()
-            return True
-        return False
+        await self.session.delete(product)
+        await self.session.commit()
+        return True
 
     async def create_category(self, category_data):
         """
