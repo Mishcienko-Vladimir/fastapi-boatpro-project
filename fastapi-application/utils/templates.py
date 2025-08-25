03566ab917e4dfaff3098e41d124f242adb4b94c
Владимир <mishchienko.2001@mail.ru>
2025-08-25 18:21:17 +0300

    Add base-dir for templates

--- fastapi-application/utils/templates.py
+++ fastapi-application/utils/templates.py
@@ -1,4 +1,6 @@
 from fastapi.templating import Jinja2Templates
 
+from core.config import BASE_DIR
 
-templates = Jinja2Templates(directory="templates")
+
+templates = Jinja2Templates(directory=BASE_DIR / "templates")
