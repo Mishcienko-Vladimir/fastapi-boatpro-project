3ab1ff94d1ed80787c4318716dd8a47d190272a9
Владимир <mishchienko.2001@mail.ru>
2025-08-25 19:28:52 +0300

    Creating func send-verification-email

---
+++ fastapi-application/mailing/send_verification_email.py
@@ -0,0 +1,52 @@
+from textwrap import dedent
+
+from core.models import User
+from mailing.send_email import send_email
+from utils import templates
+
+
+async def send_verification_email(
+    user: User,
+    verification_link: str,
+    verification_token: str,
+):
+    """
+    Отправка пользователю письма с подтверждением email.
+
+    :param user: - пользователь.
+    :param verification_link: - ссылка для подтверждения email.
+    :param verification_token: - токен для подтверждения email.
+    :return:
+    """
+
+    recipient = user.email
+    subject = f"Confirm your email for MFBoats.com"
+
+    plain_content = dedent(
+        f"""\
+        Dear {recipient},
+        
+        Please follow the link to verify your email:
+        {verification_link}
+        
+        Use this token to verify your email:
+        {verification_token}
+        
+        Thank you for using MFBoats.com!
+        © 2025 MFBoats.com
+        """
+    )
+
+    template = templates.get_template("mailing/email-verify/verification_request.html")
+    context = {
+        "user": user,
+        "verification_link": verification_link,
+        "verification_token": verification_token,
+    }
+    html_content = template.render(context)
+    await send_email(
+        recipient=recipient,
+        subject=subject,
+        plain_content=plain_content,
+        html_content=html_content,
+    )
