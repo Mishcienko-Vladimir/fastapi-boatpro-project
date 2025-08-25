6d1771a65ac3802bf8379f73ccd0196f50a4c2e9
Владимир <mishchienko.2001@mail.ru>
2025-08-25 19:29:18 +0300

    Creating func send-email-confirmed

---
+++ fastapi-application/mailing/send_email_confirmed.py
@@ -0,0 +1,38 @@
+from textwrap import dedent
+
+from core.models import User
+from mailing.send_email import send_email
+from utils import templates
+
+
+async def send_email_confirmed(user: User):
+    """
+    Отправка письма пользователю о подтверждении email.
+
+    :param user: - пользователь.
+    :return:
+    """
+
+    recipient = user.email
+    subject = "Email confirmed"
+
+    plain_content = dedent(
+        f"""\
+        Dear {recipient},
+
+        Your email has been confirmed.
+
+        Thank you for using MFBoats.com!
+        © 2025 MFBoats.com
+        """
+    )
+
+    template = templates.get_template("mailing/email-verify/email-verified.html")
+    context = {"user": user}
+    html_content = template.render(context)
+    await send_email(
+        recipient=recipient,
+        subject=subject,
+        plain_content=plain_content,
+        html_content=html_content,
+    )
