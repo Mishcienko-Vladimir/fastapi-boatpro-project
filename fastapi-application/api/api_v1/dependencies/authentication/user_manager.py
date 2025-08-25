ff5649095941c8acc539c4f23e1976ef05077735
Владимир <mishchienko.2001@mail.ru>
2025-08-25 19:24:57 +0300

    Add background-tasks

--- fastapi-application/api/api_v1/dependencies/authentication/user_manager.py
+++ fastapi-application/api/api_v1/dependencies/authentication/user_manager.py
@@ -1,15 +1,19 @@
 from typing import Annotated, TYPE_CHECKING
 
-from fastapi import Depends
+from fastapi import Depends, BackgroundTasks
 
 from core.repositories.authentication.user_manager import UserManager
 from .users import get_users_db
 
 if TYPE_CHECKING:
-    from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
+    from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase  # noqa
 
 
 async def get_user_manager(
     user_db: Annotated["SQLAlchemyUserDatabase", Depends(get_users_db)],
+    background_tasks: BackgroundTasks,
 ):
-    yield UserManager(user_db)
+    yield UserManager(
+        user_db,
+        background_tasks=background_tasks,
+    )
