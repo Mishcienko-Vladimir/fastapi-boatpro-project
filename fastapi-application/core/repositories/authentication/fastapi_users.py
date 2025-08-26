58f6734802496a17cb4ff1ccbb0ef7fef007763b
Владимир <mishchienko.2001@mail.ru>
2025-08-26 15:58:39 +0300

    Comment for current-active-verified-user

--- fastapi-application/core/repositories/authentication/fastapi_users.py
+++ fastapi-application/core/repositories/authentication/fastapi_users.py
@@ -13,4 +13,5 @@ fastapi_users = FastAPIUsers[User, UserIdType](
 )
 
 current_active_user = fastapi_users.current_user(active=True)
+# current_active_verified_user = fastapi_users.current_user(active=True, verified=True)
 current_active_superuser = fastapi_users.current_user(active=True, superuser=True)
