6c17bdf52c56172a3a7f3e0bba37c842b622adf3
Владимир <mishchienko.2001@mail.ru>
2025-08-26 11:55:34 +0300

    Add cookie-transport

--- fastapi-application/core/repositories/authentication/transport.py
+++ fastapi-application/core/repositories/authentication/transport.py
@@ -1,5 +1,16 @@
-from fastapi_users.authentication import BearerTransport
+from fastapi_users.authentication import (
+    BearerTransport,
+    CookieTransport,
+)
 
 from core.config import settings
 
-bearer_transport = BearerTransport(tokenUrl=settings.api.bearer_token_url)
+
+bearer_transport = BearerTransport(
+    tokenUrl=settings.api.bearer_token_url,
+)
+
+cookie_transport = CookieTransport(
+    cookie_max_age=settings.api.cookie_max_age,
+    cookie_secure=settings.api.cookie_secure,
+)
