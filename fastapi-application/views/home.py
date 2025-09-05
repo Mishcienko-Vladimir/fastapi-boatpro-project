e9b810ac767e4b9ea5b55d0b8b3162489ebd866c
Владимир <mishchienko.2001@mail.ru>
2025-09-05 20:31:53 +0300

    Commented everything out views

--- fastapi-application/views/home.py
+++ fastapi-application/views/home.py
@@ -1,31 +1,31 @@
-from typing import Annotated
-
-from fastapi import APIRouter, Request, Depends
-
-from core.repositories.authentication.fastapi_users import current_active_user
-from core.config import settings
-from core.models import User
-from utils.templates import templates
-
-
-router = APIRouter(
-    prefix=settings.view.home,
-)
-
-
-@router.get(
-    "/",
-    name="home",
-    include_in_schema=False,
-)
-def home(
-    request: Request,
-    user: Annotated[User, Depends(current_active_user)],
-):
-    return templates.TemplateResponse(
-        name="home.html",
-        context={
-            "request": request,
-            "user": user,
-        },
-    )
+# from typing import Annotated
+#
+# from fastapi import APIRouter, Request, Depends
+#
+# from core.repositories.authentication.fastapi_users import current_active_user
+# from core.config import settings
+# from core.models import User
+# from utils.templates import templates
+#
+#
+# router = APIRouter(
+#     prefix=settings.view.home,
+# )
+#
+#
+# @router.get(
+#     "/",
+#     name="home",
+#     include_in_schema=False,
+# )
+# def home(
+#     request: Request,
+#     user: Annotated[User, Depends(current_active_user)],
+# ):
+#     return templates.TemplateResponse(
+#         name="home.html",
+#         context={
+#             "request": request,
+#             "user": user,
+#         },
+#     )
