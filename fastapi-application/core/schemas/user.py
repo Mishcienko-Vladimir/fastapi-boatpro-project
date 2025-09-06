067ad3eb4e45f6210b3e99ace3002023e8c61bbb
Владимир <mishchienko.2001@mail.ru>
2025-09-06 19:04:21 +0300

    Add first name in user schemas

--- fastapi-application/core/schemas/user.py
+++ fastapi-application/core/schemas/user.py
@@ -1,19 +1,34 @@
 from fastapi_users import schemas
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from core.types.user_id import UserIdType
 
 
 class UserRead(schemas.BaseUser[UserIdType]):
-    pass
+
+    first_name: str = Field(
+        min_length=1,
+        max_length=50,
+        description="Имя пользователя",
+    )
 
 
 class UserCreate(schemas.BaseUserCreate):
-    pass
+
+    first_name: str = Field(
+        min_length=1,
+        max_length=50,
+        description="Имя пользователя",
+    )
 
 
 class UserUpdate(schemas.BaseUserUpdate):
-    pass
+
+    first_name: str = Field(
+        min_length=1,
+        max_length=50,
+        description="Имя пользователя",
+    )
 
 
 class UserRegisteredNotification(BaseModel):
