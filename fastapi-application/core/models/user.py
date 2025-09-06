c34333f4728de314addd64ac40783b7ee63528bb
Владимир <mishchienko.2001@mail.ru>
2025-09-06 19:02:44 +0300

    Add first name in users table

--- fastapi-application/core/models/user.py
+++ fastapi-application/core/models/user.py
@@ -2,16 +2,24 @@ from typing import TYPE_CHECKING
 
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
 from core.types.user_id import UserIdType
 from core.models.base import Base
 from core.models.mixins import IntIdPkMixin
 
 if TYPE_CHECKING:
-    from sqlalchemy.ext.asyncio import AsyncSession
+    from sqlalchemy.ext.asyncio import AsyncSession  # noqa
 
 
 class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[UserIdType]):
     """Таблица пользователей"""
+
+    first_name: Mapped[str] = mapped_column(
+        String(50),
+        comment="Имя пользователя",
+    )
 
     # Получение данных из БД
     @classmethod
