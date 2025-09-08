6fa595dd734cb33707ac20f2147de4ee9e35389d
Владимир <mishchienko.2001@mail.ru>
2025-09-08 22:01:39 +0300

    deleting comments

--- fastapi-application/views/verification.py
+++ fastapi-application/views/verification.py
@@ -1,27 +1,27 @@
-# from fastapi import APIRouter, Request
-#
-# from core.config import settings
-# from utils.templates import templates
-#
-#
-# router = APIRouter(
-#     prefix=settings.view.verify_email,
-# )
-#
-#
-# @router.get(
-#     "/",
-#     name="verify_email",
-#     include_in_schema=False,
-# )
-# def verify_email(
-#     request: Request,
-#     token: str,
-# ):
-#     return templates.TemplateResponse(
-#         name="verification.html",
-#         context={
-#             "request": request,
-#             "token": token,
-#         },
-#     )
+from fastapi import APIRouter, Request
+
+from core.config import settings
+from utils.templates import templates
+
+
+router = APIRouter(
+    prefix=settings.view.verify_email,
+)
+
+
+@router.get(
+    "/",
+    name="verify_email",
+    include_in_schema=False,
+)
+def verify_email(
+    request: Request,
+    token: str,
+):
+    return templates.TemplateResponse(
+        name="verification.html",
+        context={
+            "request": request,
+            "token": token,
+        },
+    )
