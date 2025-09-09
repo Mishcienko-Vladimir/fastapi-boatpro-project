8b3454573e5a1926da12256b077bc721389fd77e
Владимир <mishchienko.2001@mail.ru>
2025-09-09 21:04:36 +0300

    Add doc

--- fastapi-application/core/repositories/authentication/user_manager.py
+++ fastapi-application/core/repositories/authentication/user_manager.py
@@ -20,6 +20,11 @@ log = logging.getLogger(__name__)
 
 
 class UserManager(IntegerIDMixin, BaseUserManager[User, UserIdType]):
+    """
+    Класс для работы с пользователями. Делает определенные действия если произошло какое-то событие.
+    Например: при регистрации, сбросе пароля и подтверждении почты.
+    """
+
     reset_password_token_secret = settings.access_token.reset_password_token_secret
     verification_token_secret = settings.access_token.verification_token_secret
 
