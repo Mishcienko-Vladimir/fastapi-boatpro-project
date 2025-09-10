b4d8ddeee74887cebf2ac359bf8e62106eaa6199
Владимир <mishchienko.2001@mail.ru>
2025-09-10 21:36:50 +0300

    Create profile js

---
+++ fastapi-application/static/js/profile.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const logoutUrl = document.getElementById("logout-url").value;
+    const logoutBtn = document.querySelector(".btnExit");
+
+    if (logoutBtn) {
+        logoutBtn.addEventListener("click", async (event) => {
+            event.preventDefault();
+
+            try {
+                const response = await fetch(logoutUrl, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded"
+                    }
+                });
+
+                if (response.ok) {
+                    location.href = "/";
+                } else {
+                    alert("Ошибка при выходе. Попробуйте снова.");
+                }
+            } catch (error) {
+                console.error("Ошибка сети:", error);
+                alert("Ошибка сети. Попробуйте снова.");
+            }
+        });
+    }
+});
