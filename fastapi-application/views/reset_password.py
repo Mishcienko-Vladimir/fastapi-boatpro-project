94bfed09746e4ce64bcf9fd13ae76d31b1119174
Владимир <mishchienko.2001@mail.ru>
2025-09-18 19:00:36 +0300

    Create password reset views

---
+++ fastapi-application/views/reset_password.py
@@ -0,0 +1,32 @@
+from typing import Optional
+
+from fastapi import APIRouter, Request, Depends
+
+from core.repositories.authentication.fastapi_users import optional_user
+from core.config import settings
+from core.models import User
+
+from utils.templates import templates
+
+
+router = APIRouter(
+    prefix=settings.view.password_reset,
+)
+
+
+@router.get(
+    "/",
+    name="password_reset",
+    include_in_schema=False,
+)
+def password_reset(
+    request: Request,
+    user: Optional[User] = Depends(optional_user),
+):
+    return templates.TemplateResponse(
+        name="reset_password.html",
+        context={
+            "request": request,
+            "user": user,
+        },
+    )
