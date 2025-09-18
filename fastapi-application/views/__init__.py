5112c3558d68cf2f5339a03bbd62095aba722dee
Владимир <mishchienko.2001@mail.ru>
2025-09-18 19:00:44 +0300

    Init password reset views

--- fastapi-application/views/__init__.py
+++ fastapi-application/views/__init__.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 
 from .home import router as home_router
 from .verification import router as verification_router
+from .reset_password import router as reset_password_router
 
 
 router = APIRouter()
 
 router.include_router(home_router)
 router.include_router(verification_router)
+router.include_router(reset_password_router)
