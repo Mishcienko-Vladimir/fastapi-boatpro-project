005e068d48b037554f3ebdbf1c69937ac068706c
Владимир <mishchienko.2001@mail.ru>
2025-09-19 15:45:40 +0300

    Init page-missing views

--- fastapi-application/views/__init__.py
+++ fastapi-application/views/__init__.py
@@ -4,6 +4,7 @@ from .home import router as home_router
 from .verification import router as verification_router
 from .reset_password import router as reset_password_router
 from .change_password import router as change_password_router
+from .page_missing import router as page_missing_router
 
 
 router = APIRouter()
@@ -12,3 +13,4 @@ router.include_router(home_router)
 router.include_router(verification_router)
 router.include_router(reset_password_router)
 router.include_router(change_password_router)
+router.include_router(page_missing_router)
