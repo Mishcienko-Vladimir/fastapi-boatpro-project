cb954700a80d5df30f4e20734bee6e1f1a6ef812
Владимир <mishchienko.2001@mail.ru>
2025-09-19 14:22:49 +0300

    Fix code

--- fastapi-application/static/js/forgot_password.js
+++ fastapi-application/static/js/forgot_password.js
@@ -1,22 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
     const loadingIndicator = document.getElementById('loading-indicator');
-    const BtnRecoveryEmail = document.getElementById("btn-recovery-email");
+    const btnRecoveryEmail = document.getElementById("btn-recovery-email");
     const successMessage = document.getElementById("password-reset-sent");
     const errorMessage = document.getElementById("password-reset-error");
 
     const userEmailInput = document.getElementById("recovery-email");
     const resetPasswordInput = document.getElementById("reset-password-url");
 
-    if (!BtnRecoveryEmail || !userEmailInput || !resetPasswordInput) return;
+    if (!btnRecoveryEmail || !userEmailInput || !resetPasswordInput) return;
 
     const resetPasswordURL = resetPasswordInput.value;
 
-    BtnRecoveryEmail.addEventListener("click", async (event) => {
+    btnRecoveryEmail.addEventListener("click", async (event) => {
         event.preventDefault();
 
         const userEmail = userEmailInput.value.trim();
 
-        BtnRecoveryEmail.classList.add("d-none");
+        btnRecoveryEmail.classList.add("d-none");
         loadingIndicator.classList.remove("d-none");
 
         if (successMessage) successMessage.classList.add("d-none");
@@ -32,15 +32,15 @@ document.addEventListener("DOMContentLoaded", function () {
             });
 
             loadingIndicator.classList.add("d-none");
-            BtnRecoveryEmail.classList.remove("d-none");
+            btnRecoveryEmail.classList.remove("d-none");
 
             if (response.ok) {
                 if (successMessage) {
                     successMessage.classList.remove("d-none");
                 }
                 // Меняем текст кнопки
-                BtnRecoveryEmail.textContent = "Проверьте почту";
-                BtnRecoveryEmail.disabled = true;
+                btnRecoveryEmail.textContent = "Проверьте почту";
+                btnRecoveryEmail.disabled = true;
             } else {
                 // Ошибка от сервера
                 if (errorMessage) {
@@ -51,7 +51,7 @@ document.addEventListener("DOMContentLoaded", function () {
             console.error("Ошибка сети:", error);
             alert("Произошла ошибка. Попробуйте снова.");
             loadingIndicator.classList.add("d-none");
-            BtnRecoveryEmail.classList.remove("d-none");
+            btnRecoveryEmail.classList.remove("d-none");
         }
     });
 });
