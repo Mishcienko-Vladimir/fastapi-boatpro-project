cdf221dc1911e57312e3cbc52fbd29a947b7fc14
Владимир <mishchienko.2001@mail.ru>
2025-09-19 11:10:31 +0300

    Create change password views

---
+++ fastapi-application/views/change_password.py
@@ -0,0 +1,32 @@
+from typing import Optional
+
+from fastapi import APIRouter, Request, Depends
+
+from core.repositories.authentication.fastapi_users import optional_user
+from core.config import settings
+from core.models import User
+
+from utils.templates import templates
+
+
+router = APIRouter(
+    prefix=settings.view.change_password,
+)
+
+
+@router.get(
+    "/",
+    name="change_password",
+    include_in_schema=False,
+)
+def change_password(
+    request: Request,
+    user: Optional[User] = Depends(optional_user),
+):
+    return templates.TemplateResponse(
+        name="forgot-password.html",
+        context={
+            "request": request,
+            "user": user,
+        },
+    )
