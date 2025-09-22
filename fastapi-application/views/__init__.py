9c74a201c605d8929ad11b77278040fcc1dd1bf2
Владимир <mishchienko.2001@mail.ru>
2025-09-22 16:52:28 +0300

    Init catalog views

--- fastapi-application/views/__init__.py
+++ fastapi-application/views/__init__.py
@@ -5,6 +5,7 @@ from .verification import router as verification_router
 from .reset_password import router as reset_password_router
 from .change_password import router as change_password_router
 from .page_missing import router as page_missing_router
+from .catalog import router as catalog_router
 
 
 router = APIRouter()
@@ -14,3 +15,4 @@ router.include_router(verification_router)
 router.include_router(reset_password_router)
 router.include_router(change_password_router)
 router.include_router(page_missing_router)
+router.include_router(catalog_router)
