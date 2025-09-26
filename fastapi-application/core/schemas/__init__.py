52da29fb97efcc6466e15a144a5ecac98cf668b7
Владимир <mishchienko.2001@mail.ru>
2025-09-26 17:50:40 +0300

    Init favorite-model schema

--- fastapi-application/core/schemas/__init__.py
+++ fastapi-application/core/schemas/__init__.py
@@ -1,15 +1,19 @@
 __add__ = (
+    "BaseSchemaModel",
+    "FavoriteModel",
     "UserRegisteredNotification",
     "UserCreate",
     "UserUpdate",
     "UserRead",
-    "BaseSchemaModel",
+    "UserFavorites",
 )
 
+from .base_model import BaseSchemaModel
+from .favorite import FavoriteModel
 from .user import (
     UserRegisteredNotification,
     UserCreate,
     UserUpdate,
     UserRead,
+    UserFavorites,
 )
-from .base_model import BaseSchemaModel
