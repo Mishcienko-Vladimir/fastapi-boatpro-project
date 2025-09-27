2b7a7ab0fb9f1e1ef52ae45a59fc9e62cdf1c661
Владимир <mishchienko.2001@mail.ru>
2025-09-27 15:28:13 +0300

    Create user-manager-crud

---
+++ fastapi-application/core/repositories/user_manager_crud.py
@@ -0,0 +1,41 @@
+from typing import Sequence
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models import User
+
+
+class UserManagerCrud:
+    """
+    Помощник для работы с пользователями.
+
+    :session: - сессия для работы с БД.
+
+    :methods:
+        - get_all_users - получает всех пользователей.
+        - get_user_by_id - получает пользователя по id.
+    """
+
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def get_all_users(self) -> Sequence[User]:
+        """
+        Получает всех пользователей.
+
+        :return: - список всех пользователей.
+        """
+
+        stmt = select(User).order_by(User.id)
+        result = await self.session.scalars(stmt)
+        return result.all()
+
+    async def get_user_by_id(self, user_id: int) -> User | None:
+        """
+        Получает пользователя по id.
+
+        :param user_id: - id пользователя.
+        :return: - пользователь или None.
+        """
+        return await self.session.get(User, user_id)
