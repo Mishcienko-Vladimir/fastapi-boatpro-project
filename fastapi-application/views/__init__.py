fd39e3dd86f9094cf71a647e587cafad61e6e46d
Владимир <mishchienko.2001@mail.ru>
2025-09-29 21:47:25 +0300

    Init favorites views

--- fastapi-application/views/__init__.py
+++ fastapi-application/views/__init__.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter
 
 from .home import router as home_router
+from .page_missing import router as page_missing_router
+from .favorites import router as favorites_router
+
 from views.auth.verification import router as verification_router
 from views.auth.reset_password import router as reset_password_router
 from views.auth.change_password import router as change_password_router
-from .page_missing import router as page_missing_router
+
 from views.products.catalog import router as catalog_router
 from views.products.boats import router as boats_router
 
@@ -12,9 +15,10 @@ from views.products.boats import router as boats_router
 router = APIRouter()
 
 router.include_router(home_router)
+router.include_router(page_missing_router)
+router.include_router(favorites_router)
 router.include_router(verification_router)
 router.include_router(reset_password_router)
 router.include_router(change_password_router)
-router.include_router(page_missing_router)
 router.include_router(catalog_router)
 router.include_router(boats_router)
