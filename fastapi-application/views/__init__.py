6e19a4e28e8ba8f544e1e74bef7e9aefe9e505f1
Владимир <mishchienko.2001@mail.ru>
2025-10-15 21:33:28 +0300

    Init search view

--- fastapi-application/views/__init__.py
+++ fastapi-application/views/__init__.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from .home import router as home_router
 from .page_missing import router as page_missing_router
 from .favorites import router as favorites_router
+from .search import router as search_router
 
 from views.auth.verification import router as verification_router
 from views.auth.reset_password import router as reset_password_router
@@ -19,6 +20,7 @@ router = APIRouter()
 router.include_router(home_router)
 router.include_router(page_missing_router)
 router.include_router(favorites_router)
+router.include_router(search_router)
 router.include_router(verification_router)
 router.include_router(reset_password_router)
 router.include_router(change_password_router)
