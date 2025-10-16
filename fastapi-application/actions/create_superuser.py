345a48051e6b2e9a45bacc21e7e881d566cbc5af
Владимир <mishchienko.2001@mail.ru>
2025-10-16 20:22:36 +0300

    Fix superuser

--- fastapi-application/actions/create_superuser.py
+++ fastapi-application/actions/create_superuser.py
@@ -2,6 +2,7 @@ import asyncio
 import contextlib
 
 from os import getenv
+from fastapi import BackgroundTasks
 
 from api.api_v1.dependencies.authentication import get_users_db
 from api.api_v1.dependencies.authentication import get_user_manager
@@ -12,10 +13,13 @@ from core.schemas.user import UserCreate
 
 
 get_users_db_context = contextlib.asynccontextmanager(get_users_db)
-get_user_manager_context = contextlib.asynccontextmanager(get_user_manager)
+get_user_manager_context = contextlib.asynccontextmanager(
+    lambda db, _: get_user_manager(db, BackgroundTasks())
+)
 
 default_email = getenv("DEFAULT_EMAIL", "admin@admin.com")
-default_password = getenv("DEFAULT_PASSWORD", "abc")
+default_password = getenv("DEFAULT_PASSWORD", "password")
+default_first_name = getenv("DEFAULT_FIRST_NAME", "Admin")
 default_is_active = True
 default_is_superuser = True
 default_is_verified = True
@@ -35,6 +39,7 @@ async def create_user(
 async def create_superuser(
     email: str = default_email,
     password: str = default_password,
+    first_name: str = default_first_name,
     is_active: bool = default_is_active,
     is_superuser: bool = default_is_superuser,
     is_verified: bool = default_is_verified,
@@ -42,6 +47,7 @@ async def create_superuser(
     user_create = UserCreate(
         email=email,
         password=password,
+        first_name=first_name,
         is_active=is_active,
         is_superuser=is_superuser,
         is_verified=is_verified,
@@ -49,7 +55,7 @@ async def create_superuser(
 
     async with db_helper.session_factory() as session:
         async with get_users_db_context(session) as users_db:
-            async with get_user_manager_context(users_db) as user_manager:
+            async with get_user_manager_context(users_db, None) as user_manager:
                 return await create_user(
                     user_manager=user_manager,
                     user_create=user_create,
