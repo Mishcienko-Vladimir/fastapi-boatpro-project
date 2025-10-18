2ec333d2d1aa0c3897e46924133d94137788cfe2
Владимир <mishchienko.2001@mail.ru>
2025-10-18 19:04:28 +0300

    Add http-exception-handler

--- fastapi-application/errors_handlers.py
+++ fastapi-application/errors_handlers.py
@@ -1,12 +1,13 @@
 # Обработчик ошибок
-
 import logging
 
-from fastapi import FastAPI, Request, status
+from fastapi import FastAPI, Request, status, HTTPException
 from fastapi.responses import ORJSONResponse
 
 from pydantic import ValidationError
 from sqlalchemy.exc import DatabaseError
+
+from utils.templates import templates
 
 
 log = logging.getLogger(__name__)
@@ -45,5 +46,15 @@ def register_errors_handlers(app: FastAPI) -> None:
             content={
                 "message": "Произошла непредвиденная ошибка. "
                 "Администраторы уже работают над решением."
+            },
+        )
+
+    @app.exception_handler(HTTPException)
+    def http_exception_handler(request: Request):
+        return templates.TemplateResponse(
+            name="page-missing.html",
+            context={
+                "request": request,
+                "user": None,
             },
         )
