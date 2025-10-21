42cc7b0cd609bba893bfcc0bbb1808099f7f5979
Владимир <mishchienko.2001@mail.ru>
2025-10-21 16:42:16 +0300

    Add admin-boats views

--- fastapi-application/views/admin.py
+++ fastapi-application/views/admin.py
@@ -1,10 +1,18 @@
 from typing import Annotated
-
 from fastapi import APIRouter, Request, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from api.api_v1.routers.products.boats import (
+    get_boats,
+    create_boat,
+    update_boat_data_by_id,
+    update_boat_images_by_id,
+    delete_boat_by_id,
+)
 
 from core.repositories.authentication.fastapi_users import current_active_superuser
 from core.config import settings
-from core.models import User
+from core.models import User, db_helper
 
 from utils.templates import templates
 
@@ -32,5 +40,30 @@ def admin(
         context={
             "request": request,
             "user": user,
+        },
+    )
+
+
+@router.get(
+    path=settings.view.boats,
+    name="admin_boats",
+    include_in_schema=False,
+    response_model=None,
+)
+async def admin_boats(
+    request: Request,
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    user: Annotated[
+        User,
+        Depends(current_active_superuser),
+    ],
+):
+    boats_list = await get_boats(session=session)
+    return templates.TemplateResponse(
+        name="admin/boats.html",
+        context={
+            "request": request,
+            "user": user,
+            "boats_list": boats_list,
         },
     )
