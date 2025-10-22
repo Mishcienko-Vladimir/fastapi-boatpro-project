fa43cae3dbfcc14edf553fb5f193a376b45a00d3
Владимир <mishchienko.2001@mail.ru>
2025-10-22 14:53:09 +0300

    Add admin-create-boat view

--- fastapi-application/views/admin.py
+++ fastapi-application/views/admin.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from fastapi import Form, HTTPException
+from fastapi import Form, HTTPException, File, UploadFile
 from fastapi import APIRouter, Request, Depends
 
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -92,6 +92,79 @@ async def admin_delete_boat(
         message = f"Катер с ID {boat_id} успешно удален"
     except HTTPException as exc:
         message = exc.detail
+
+    return templates.TemplateResponse(
+        name="admin/boats.html",
+        context={
+            "request": request,
+            "user": user,
+            "boats_list": await get_boats(session=session),
+            "message": message,
+        },
+    )
+
+
+@router.post(
+    path=f"{settings.view.boats}/create-boat",
+    name="admin_create_boat",
+    include_in_schema=False,
+    response_model=None,
+)
+async def admin_create_boat(
+    request: Request,
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    user: Annotated[
+        User,
+        Depends(current_active_superuser),
+    ],
+    category_id: int = Form(...),
+    name: str = Form(...),
+    price: int = Form(...),
+    company_name: str = Form(...),
+    description: str = Form(...),
+    is_active: bool = Form(...),
+    length_hull: int = Form(...),
+    width_hull: int = Form(...),
+    weight: int = Form(...),
+    capacity: int = Form(...),
+    maximum_load: int = Form(...),
+    hull_material: str = Form(...),
+    thickness_side_sheet: int | None = Form(0),
+    bottom_sheet_thickness: int | None = Form(0),
+    fuel_capacity: int | None = Form(0),
+    maximum_engine_power: int | None = Form(0),
+    height_side_midship: int | None = Form(0),
+    transom_height: int | None = Form(0),
+    images: list[UploadFile] = File(...),
+):
+    try:
+        new_boat = await create_boat(
+            session=session,
+            category_id=category_id,
+            name=name,
+            price=price,
+            company_name=company_name,
+            description=description,
+            is_active=is_active,
+            length_hull=length_hull,
+            width_hull=width_hull,
+            weight=weight,
+            capacity=capacity,
+            maximum_load=maximum_load,
+            hull_material=hull_material,
+            thickness_side_sheet=thickness_side_sheet,
+            bottom_sheet_thickness=bottom_sheet_thickness,
+            fuel_capacity=fuel_capacity,
+            maximum_engine_power=maximum_engine_power,
+            height_side_midship=height_side_midship,
+            transom_height=transom_height,
+            images=images,
+        )
+        message = f"Катер с ID {new_boat.id} успешно создан"
+    except HTTPException as exc:
+        message = "Катер с именем {name} уже существует"
+    except Exception as exc:
+        message = "Ошибка при создании катера"
 
     return templates.TemplateResponse(
         name="admin/boats.html",
