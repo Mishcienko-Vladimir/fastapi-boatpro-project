4969e264472864a5647fc4ffb2f04d3d8fea6262
Владимир <mishchienko.2001@mail.ru>
2025-10-22 17:48:04 +0300

    Add admin_update_boat view

--- fastapi-application/views/admin.py
+++ fastapi-application/views/admin.py
@@ -1,4 +1,4 @@
-from typing import Annotated
+from typing import Annotated, Optional
 
 from fastapi import Form, HTTPException, File, UploadFile
 from fastapi import APIRouter, Request, Depends
@@ -16,6 +16,7 @@ from api.api_v1.routers.products.boats import (
 from core.repositories.authentication.fastapi_users import current_active_superuser
 from core.config import settings
 from core.models import User, db_helper
+from core.schemas.products.boat import BoatUpdate
 
 from utils.templates import templates
 
@@ -85,11 +86,11 @@ async def admin_delete_boat(
         User,
         Depends(current_active_superuser),
     ],
-    boat_id: int = Form(...),
+    boat_id_del: int = Form(...),
 ):
     try:
-        await delete_boat_by_id(session=session, boat_id=boat_id)
-        message = f"Катер с ID {boat_id} успешно удален"
+        await delete_boat_by_id(session=session, boat_id=boat_id_del)
+        message = f"Катер с ID {boat_id_del} успешно удален"
     except HTTPException as exc:
         message = exc.detail
 
@@ -162,9 +163,84 @@ async def admin_create_boat(
         )
         message = f"Катер с ID {new_boat.id} успешно создан"
     except HTTPException as exc:
-        message = "Катер с именем {name} уже существует"
+        message = f"Катер с именем {name} уже существует"
     except Exception as exc:
         message = "Ошибка при создании катера"
+
+    return templates.TemplateResponse(
+        name="admin/boats.html",
+        context={
+            "request": request,
+            "user": user,
+            "boats_list": await get_boats(session=session),
+            "message": message,
+        },
+    )
+
+
+@router.post(
+    path=f"{settings.view.boats}/update-boat",
+    name="admin_update_boat",
+    include_in_schema=False,
+    response_model=None,
+)
+async def admin_update_boat(
+    request: Request,
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    user: Annotated[
+        User,
+        Depends(current_active_superuser),
+    ],
+    boat_id_up: int = Form(...),
+    name: Optional[str] = Form(None),
+    price: Optional[str] = Form(None),
+    company_name: Optional[str] = Form(None),
+    description: Optional[str] = Form(None),
+    is_active: Optional[bool] = Form(None),
+    length_hull: Optional[str] = Form(None),
+    width_hull: Optional[str] = Form(None),
+    weight: Optional[str] = Form(None),
+    capacity: Optional[str] = Form(None),
+    maximum_load: Optional[str] = Form(None),
+    hull_material: Optional[str] = Form(None),
+    thickness_side_sheet: Optional[str] = Form(None),
+    bottom_sheet_thickness: Optional[str] = Form(None),
+    fuel_capacity: Optional[str] = Form(None),
+    maximum_engine_power: Optional[str] = Form(None),
+    height_side_midship: Optional[str] = Form(None),
+    transom_height: Optional[str] = Form(None),
+):
+    try:
+        update_data = {}
+        for key, value in locals().items():
+            if (
+                key not in ["request", "session", "user", "boat_id_up"]
+                and value is not None
+            ):
+                # Пропускаем пустые строки
+                if isinstance(value, str) and value.strip() == "":
+                    continue
+                try:
+                    # Пытаемся конвертировать в int, если возможно
+                    numeric_value = int(value)
+                    update_data[key] = numeric_value
+                except (ValueError, TypeError):
+                    update_data[key] = value
+
+        if not update_data:
+            message = "Нет данных для обновления"
+
+        boat_update = BoatUpdate(**update_data)
+        updated_boat = await update_boat_data_by_id(
+            session=session,
+            boat_id=boat_id_up,
+            boat_data=boat_update,
+        )
+        message = f"Катер с ID {boat_id_up} успешно обновлен"
+    except HTTPException as exc:
+        message = f"Катер с ID {boat_id_up} не найден"
+    except Exception as exc:
+        message = "Ошибка при обновлении катера"
 
     return templates.TemplateResponse(
         name="admin/boats.html",
