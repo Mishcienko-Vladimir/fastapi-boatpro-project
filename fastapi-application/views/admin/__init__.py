d5a082aaff48ea3cd5bc671d436584af8dd68b46
Владимир <mishchienko.2001@mail.ru>
2025-10-23 12:37:20 +0300

    Init views outboard-motor and trailer for admin-panel

--- fastapi-application/views/admin/__init__.py
+++ fastapi-application/views/admin/__init__.py
@@ -3,9 +3,13 @@ from fastapi import APIRouter
 from core.config import settings
 from .home import router as home_router
 from .boats import router as boats_router
+from .outboard_motors import router as outboard_motors_router
+from .trailers import router as trailers_router
 
 
 router = APIRouter(prefix=settings.view.admin)
 
 router.include_router(home_router)
 router.include_router(boats_router)
+router.include_router(outboard_motors_router)
+router.include_router(trailers_router)
