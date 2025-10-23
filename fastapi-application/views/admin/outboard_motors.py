348342dcf4f95a2c26a06dbe332d16424fbf025d
Владимир <mishchienko.2001@mail.ru>
2025-10-23 13:32:20 +0300

    Add view admin_create_outboard_motor

--- fastapi-application/views/admin/outboard_motors.py
+++ fastapi-application/views/admin/outboard_motors.py
@@ -87,3 +87,66 @@ async def admin_delete_outboard_motor(
             "message": message,
         },
     )
+
+
+@router.post(
+    path="/create-outboard-motor",
+    name="admin_create_outboard_motor",
+    include_in_schema=False,
+    response_model=None,
+)
+async def admin_create_outboard_motor(
+    request: Request,
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    user: Annotated[
+        User,
+        Depends(current_active_superuser),
+    ],
+    category_id: int = Form(...),
+    name: str = Form(...),
+    price: int = Form(...),
+    company_name: str = Form(...),
+    description: str = Form(...),
+    is_active: bool = Form(...),
+    engine_power: int = Form(...),
+    engine_type: EngineType = Form(...),
+    weight: int = Form(...),
+    number_cylinders: int = Form(...),
+    engine_displacement: int = Form(...),
+    control_type: ControlType = Form(...),
+    starter_type: StarterType = Form(...),
+    images: list[UploadFile] = File(...),
+):
+    try:
+        new_outboard_motor = await create_outboard_motor(
+            session=session,
+            category_id=category_id,
+            name=name,
+            price=price,
+            company_name=company_name,
+            description=description,
+            is_active=is_active,
+            engine_power=engine_power,
+            engine_type=engine_type,
+            weight=weight,
+            number_cylinders=number_cylinders,
+            engine_displacement=engine_displacement,
+            control_type=control_type,
+            starter_type=starter_type,
+            images=images,
+        )
+        message = f"Прицеп с ID {new_outboard_motor.id} успешно создан"
+    except HTTPException as exc:
+        message = f"Прицеп с именем {name} уже существует"
+    except Exception as exc:
+        message = "Ошибка при создании прицепа"
+
+    return templates.TemplateResponse(
+        name="admin/outboard_motors.html",
+        context={
+            "request": request,
+            "user": user,
+            "outboard_motors_list": await get_outboard_motors(session=session),
+            "message": message,
+        },
+    )
