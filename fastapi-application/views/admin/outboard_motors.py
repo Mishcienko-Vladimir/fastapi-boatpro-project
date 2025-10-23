af581e8d71dfa02ec544cb0bb567d955f4a23c1c
Владимир <mishchienko.2001@mail.ru>
2025-10-23 13:33:33 +0300

    Add view admin_update_outboard_motor

--- fastapi-application/views/admin/outboard_motors.py
+++ fastapi-application/views/admin/outboard_motors.py
@@ -150,3 +150,71 @@ async def admin_create_outboard_motor(
             "message": message,
         },
     )
+
+
+@router.post(
+    path="/update-outboard-motor",
+    name="admin_update_outboard_motor",
+    include_in_schema=False,
+    response_model=None,
+)
+async def admin_update_outboard_motor(
+    request: Request,
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    user: Annotated[
+        User,
+        Depends(current_active_superuser),
+    ],
+    outboard_motor_id_up: int = Form(...),
+    name: Optional[str] = Form(None),
+    price: Optional[str] = Form(None),
+    company_name: Optional[str] = Form(None),
+    description: Optional[str] = Form(None),
+    is_active: Optional[bool] = Form(None),
+    engine_power: Optional[str] = Form(None),
+    engine_type: Optional[EngineType] = Form(None),
+    weight: Optional[str] = Form(None),
+    number_cylinders: Optional[str] = Form(None),
+    engine_displacement: Optional[str] = Form(None),
+    control_type: Optional[ControlType] = Form(None),
+    starter_type: Optional[StarterType] = Form(None),
+):
+    try:
+        update_data = {}
+        for key, value in locals().items():
+            if (
+                key not in ["request", "session", "user", "outboard_motor_id_up"]
+                and value is not None
+            ):
+                if isinstance(value, str) and value.strip() == "":
+                    continue
+                try:
+                    numeric_value = int(value)
+                    update_data[key] = numeric_value
+                except (ValueError, TypeError):
+                    update_data[key] = value
+
+        if not update_data:
+            message = "Нет данных для обновления"
+
+        outboard_motor_update = OutboardMotorUpdate(**update_data)
+        updated_outboard_motor = await update_outboard_motor_data_by_id(
+            session=session,
+            outboard_motor_id=outboard_motor_id_up,
+            outboard_motor_data=outboard_motor_update,
+        )
+        message = f"Лодочный мотор с ID {outboard_motor_id_up} успешно обновлен"
+    except HTTPException as exc:
+        message = f"Лодочный мотор с ID {outboard_motor_id_up} не найден"
+    except Exception as exc:
+        message = "Ошибка при обновлении лодочного мотора"
+
+    return templates.TemplateResponse(
+        name="admin/outboard_motors.html",
+        context={
+            "request": request,
+            "user": user,
+            "outboard_motors_list": await get_outboard_motors(session=session),
+            "message": message,
+        },
+    )
