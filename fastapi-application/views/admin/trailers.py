1b75260611b5964df6c50d31fd30172c311e9062
Владимир <mishchienko.2001@mail.ru>
2025-10-23 13:05:38 +0300

    Add view admin_trailers

--- fastapi-application/views/admin/trailers.py
+++ fastapi-application/views/admin/trailers.py
@@ -22,3 +22,28 @@ from utils.templates import templates
 
 
 router = APIRouter(prefix=settings.view.trailers)
+
+
+@router.get(
+    path="/",
+    name="admin_trailers",
+    include_in_schema=False,
+    response_model=None,
+)
+async def admin_trailers(
+    request: Request,
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    user: Annotated[
+        User,
+        Depends(current_active_superuser),
+    ],
+):
+    trailers_list = await get_trailers(session=session)
+    return templates.TemplateResponse(
+        name="admin/trailers.html",
+        context={
+            "request": request,
+            "user": user,
+            "trailers_list": trailers_list,
+        },
+    )
