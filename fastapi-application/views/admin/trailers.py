bb6ca342604c193ad4f6df4307963fad7d8d3fad
Владимир <mishchienko.2001@mail.ru>
2025-10-23 13:07:06 +0300

    Add view admin_create_trailer

--- fastapi-application/views/admin/trailers.py
+++ fastapi-application/views/admin/trailers.py
@@ -79,3 +79,60 @@ async def admin_delete_trailer(
             "message": message,
         },
     )
+
+
+@router.post(
+    path="/create-trailer",
+    name="admin_create_trailer",
+    include_in_schema=False,
+    response_model=None,
+)
+async def admin_create_trailer(
+    request: Request,
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    user: Annotated[
+        User,
+        Depends(current_active_superuser),
+    ],
+    category_id: int = Form(...),
+    name: str = Form(...),
+    price: int = Form(...),
+    company_name: str = Form(...),
+    description: str = Form(...),
+    is_active: bool = Form(...),
+    full_mass: int = Form(...),
+    load_capacity: int = Form(...),
+    trailer_length: int = Form(...),
+    max_ship_length: int = Form(...),
+    images: list[UploadFile] = File(...),
+):
+    try:
+        new_trailer = await create_trailer(
+            session=session,
+            category_id=category_id,
+            name=name,
+            price=price,
+            company_name=company_name,
+            description=description,
+            is_active=is_active,
+            full_mass=full_mass,
+            load_capacity=load_capacity,
+            trailer_length=trailer_length,
+            max_ship_length=max_ship_length,
+            images=images,
+        )
+        message = f"Прицеп с ID {new_trailer.id} успешно создан"
+    except HTTPException as exc:
+        message = f"Прицеп с именем {name} уже существует"
+    except Exception as exc:
+        message = "Ошибка при создании прицепа"
+
+    return templates.TemplateResponse(
+        name="admin/trailers.html",
+        context={
+            "request": request,
+            "user": user,
+            "trailers_list": await get_trailers(session=session),
+            "message": message,
+        },
+    )
