124e0ec557a308cb0e5c6bcbf70671dd88325d7d
Владимир <mishchienko.2001@mail.ru>
2025-10-24 17:45:47 +0300

    Refactoring admin-home view

--- fastapi-application/views/admin/home.py
+++ fastapi-application/views/admin/home.py
@@ -1,10 +1,14 @@
 from typing import Annotated
-
 from fastapi import APIRouter, Request, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from api.api_v1.services.products import ProductsService
 
 from core.repositories.authentication.fastapi_users import current_active_superuser
 from core.config import settings
-from core.models import User
+from core.models import User, db_helper
+from core.models.products import Product
+from core.schemas.products import ProductBaseModelRead
 
 from utils.templates import templates
 
@@ -18,17 +22,37 @@ router = APIRouter(prefix=settings.view.home)
     include_in_schema=False,
     response_model=None,
 )
-def admin_home(
+async def admin_home(
     request: Request,
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
     user: Annotated[
         User,
         Depends(current_active_superuser),
     ],
 ):
+    all_products = await ProductsService(
+        session=session, product_db=Product
+    ).get_products()
+    products_list = [
+        ProductBaseModelRead.model_validate(product) for product in all_products
+    ]
+    number_boats = sum(1 for boat in products_list if boat.type_product == "boat")
+    number_outboard_motors = sum(
+        1
+        for outboard_motor in products_list
+        if outboard_motor.type_product == "outboard_motor"
+    )
+    number_trailers = sum(
+        1 for trailer in products_list if trailer.type_product == "trailer"
+    )
     return templates.TemplateResponse(
         name="admin/home.html",
         context={
             "request": request,
             "user": user,
+            "products_list": products_list,
+            "number_boats": number_boats,
+            "number_outboard_motors": number_outboard_motors,
+            "number_trailers": number_trailers,
         },
     )
