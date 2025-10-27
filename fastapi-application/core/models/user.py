bffd4a0936ae598cd31fe94de288970159aef39b
Владимир <mishchienko.2001@mail.ru>
2025-10-27 17:43:47 +0300

    Add new method for SQLAlchemyUserDatabase

--- fastapi-application/core/models/user.py
+++ fastapi-application/core/models/user.py
@@ -1,8 +1,11 @@
 from typing import TYPE_CHECKING
 
-from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from fastapi_users_db_sqlalchemy import (
+    SQLAlchemyBaseUserTable,
+    SQLAlchemyUserDatabase as SQLAlchemyUserDatabaseGeneric,
+)
 
-from sqlalchemy import String
+from sqlalchemy import String, select
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from core.types.user_id import UserIdType
@@ -12,6 +15,20 @@ from core.models.mixins import IntIdPkMixin
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession  # noqa
     from core.models.favorite import Favorite  # noqa
+
+
+class SQLAlchemyUserDatabase(SQLAlchemyUserDatabaseGeneric):
+    """
+    Добавление новых методов в SQLAlchemyUserDatabase, для работы с пользователями.
+
+    :methods:
+        - get_users() - возвращает список пользователей.
+    """
+
+    async def get_users(self) -> list["User"]:
+        statement = select(User).order_by(User.id)
+        results = await self.session.scalars(statement)
+        return list(results.all())
 
 
 class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[UserIdType]):
