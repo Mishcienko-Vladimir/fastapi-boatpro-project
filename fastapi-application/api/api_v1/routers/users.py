93fcac6403063ade972ef0c81a440dc89eb4fba0
Владимир <mishchienko.2001@mail.ru>
2025-10-27 17:59:17 +0300

    Creating key-builder users-list-key-builder

--- fastapi-application/api/api_v1/routers/users.py
+++ fastapi-application/api/api_v1/routers/users.py
@@ -1,7 +1,14 @@
-from fastapi import APIRouter
+import hashlib
+
 from typing import (
     Annotated,
+    Any,
+    Callable,
+    Dict,
+    Optional,
+    Tuple,
 )
+
 from fastapi import APIRouter, Depends, Response, Request
 from api.api_v1.dependencies.authentication import get_users_db
 from core.repositories.authentication.fastapi_users import fastapi_users
@@ -12,6 +19,31 @@ from core.schemas.user import UserRead, UserUpdate
 
 
 router = APIRouter(prefix=settings.api.v1.users, tags=["Users"])
+
+
+def users_list_key_builder(
+    func: Callable[..., Any],
+    namespace: str,
+    *,
+    request: Optional[Request] = None,
+    response: Optional[Response] = None,
+    args: Tuple[Any, ...],
+    kwargs: Dict[str, Any],
+) -> str:
+    """
+    Функция для построения ключа кэша для списка пользователей.
+    """
+    exclude_types = (SQLAlchemyUserDatabase,)
+    cache_kw = {}
+    for name, value in kwargs.items():
+        if isinstance(value, exclude_types):
+            continue
+        cache_kw[name] = value
+
+    cache_key = hashlib.md5(  # noqa: S324
+        f"{func.__module__}:{func.__name__}:{args}:{cache_kw}".encode()
+    ).hexdigest()
+    return f"{namespace}:{cache_key}"
 
 
 @router.get(
