1e2022bbf178d6a1bb6979d1bb02a55b9ceb4ade
Владимир <mishchienko.2001@mail.ru>
2025-10-28 21:39:09 +0300

    Creating func get_by_id_key_builder

--- fastapi-application/utils/key_builder.py
+++ fastapi-application/utils/key_builder.py
@@ -66,3 +66,36 @@ def get_by_name_key_builder(
     cache_key = hashlib.md5(key_str.encode()).hexdigest()
 
     return f"{namespace}:{cache_key}"
+
+
+def get_by_id_key_builder(
+    func: Callable[..., Any],
+    namespace: str,
+    *,
+    request: Optional[Request] = None,
+    response: Optional[Any] = None,
+    args: Tuple[Any, ...],
+    kwargs: Dict[str, Any],
+) -> str:
+    """
+    Формирует уникальный ключ кэша для метода get_by_id.
+    Учитывает id товара и тип ресурса.
+    """
+
+    exclude_types = (AsyncSession,)
+    filtered_kwargs = {
+        k: v for k, v in kwargs.items() if not isinstance(v, exclude_types)
+    }
+
+    id_param = next((v for k, v in filtered_kwargs.items() if "id" in k), None)
+
+    if id_param is None:
+        raise ValueError("Не найден параметр 'id' в запросе")
+
+    path = request.scope.get("path", "") if request else ""
+    method = request.scope.get("method", "GET") if request else ""
+
+    key_str = f"{func.__module__}:{func.__name__}:{method}:{path}:{id_param}"
+    cache_key = hashlib.md5(key_str.encode()).hexdigest()
+
+    return f"{namespace}:{cache_key}"
