12cb4a5dc18a8c027425714ecb8c778dd86c796b
Владимир <mishchienko.2001@mail.ru>
2025-10-28 16:19:45 +0300

    Init universal-list-key-builder

--- fastapi-application/utils/__init__.py
+++ fastapi-application/utils/__init__.py
@@ -1,7 +1,9 @@
 __all__ = (
     "camel_case_to_snake_case",
+    "universal_list_key_builder",
     "templates",
 )
 
 from .case_converter import camel_case_to_snake_case
+from .key_builder import universal_list_key_builder
 from .templates import templates
