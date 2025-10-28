31fc62cc0a562ce5c66e11182194d8fb9a87b8f8
Владимир <mishchienko.2001@mail.ru>
2025-10-28 16:18:06 +0300

    Creating func format-datetime for templates

--- fastapi-application/utils/templates.py
+++ fastapi-application/utils/templates.py
@@ -1,6 +1,34 @@
 from fastapi.templating import Jinja2Templates
+from datetime import datetime
 
 from core.config import BASE_DIR
 
 
-templates = Jinja2Templates(directory=BASE_DIR / "templates")
+def format_datetime(value: str | datetime) -> str:
+    """
+    Форматирует дату в формате YYYY-MM-DD HH:MM.
+    Поддерживает строки в формате ISO (например, '2025-10-07T12:25:49.158480').
+    """
+    if isinstance(value, str):
+        try:
+            # Пробуем преобразовать строку в datetime
+            dt = datetime.fromisoformat(value.replace("Z", "+00:00"))
+        except ValueError:
+            return value  # Возвращаем исходную строку, если не удалось преобразовать
+    elif isinstance(value, datetime):
+        dt = value
+    else:
+        return ""
+
+    return dt.strftime("%Y-%m-%d %H:%M")
+
+
+class AppTemplates(Jinja2Templates):
+    """Добавляет дополнительные фильтры для шаблонов Jinja2Templates."""
+
+    def __init__(self, directory):
+        super().__init__(directory=directory)
+        self.env.filters["format_datetime"] = format_datetime
+
+
+templates = AppTemplates(directory=BASE_DIR / "templates")
