9acc9a68482a9928d7c0a0fb53e34d9375ad8e71
Владимир <mishchienko.2001@mail.ru>
2025-10-28 21:39:17 +0300

    Init func get_by_id_key_builder

--- fastapi-application/utils/__init__.py
+++ fastapi-application/utils/__init__.py
@@ -2,6 +2,7 @@ __all__ = (
     "camel_case_to_snake_case",
     "universal_list_key_builder",
     "get_by_name_key_builder",
+    "get_by_id_key_builder",
     "templates",
 )
 
@@ -9,5 +10,6 @@ from .case_converter import camel_case_to_snake_case
 from .key_builder import (
     universal_list_key_builder,
     get_by_name_key_builder,
+    get_by_id_key_builder,
 )
 from .templates import templates
