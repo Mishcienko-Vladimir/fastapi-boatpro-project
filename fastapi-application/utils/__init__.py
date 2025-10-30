71ad7f7a0652d5fe127b356317be823d95de4b14
Владимир <mishchienko.2001@mail.ru>
2025-10-30 20:44:13 +0300

    Remove key-builder in init

--- fastapi-application/utils/__init__.py
+++ fastapi-application/utils/__init__.py
@@ -1,15 +1,7 @@
 __all__ = (
     "camel_case_to_snake_case",
-    "universal_list_key_builder",
-    "get_by_name_key_builder",
-    "get_by_id_key_builder",
     "templates",
 )
 
 from .case_converter import camel_case_to_snake_case
-from .key_builder import (
-    universal_list_key_builder,
-    get_by_name_key_builder,
-    get_by_id_key_builder,
-)
 from .templates import templates
