80046cc66419a2e8ab96a4fc2e6749a08d7a2d3b
Владимир <mishchienko.2001@mail.ru>
2025-10-30 20:44:35 +0300

    Add func users_list_key_builder

--- fastapi-application/utils/key_builder.py
+++ fastapi-application/utils/key_builder.py
@@ -7,7 +7,9 @@ from typing import (
     Tuple,
 )
 from sqlalchemy.ext.asyncio import AsyncSession
-from fastapi import Request
+from fastapi import Request, Response
+
+from core.models.user import SQLAlchemyUserDatabase
 
 
 def universal_list_key_builder(
@@ -30,6 +32,69 @@ def universal_list_key_builder(
     method = request.scope.get("method", "GET") if request else ""
 
     key_str = f"{func.__module__}:{func.__name__}:{method}:{path}:{cache_kw}"
+    cache_key = hashlib.md5(key_str.encode()).hexdigest()
+
+    return f"{namespace}:{cache_key}"
+
+
+def users_list_key_builder(
+    func: Callable[..., Any],
+    namespace: str,
+    *,
+    request: Optional[Request] = None,
+    response: Optional[Response] = None,
+    args: Tuple[Any, ...],
+    kwargs: Dict[str, Any],
+) -> str:
+    """
+    Функция для построения ключа кэша для списка пользователей.
+    """
+    exclude_types = (SQLAlchemyUserDatabase,)
+    cache_kw = {}
+    for name, value in kwargs.items():
+        if isinstance(value, exclude_types):
+            continue
+        cache_kw[name] = value
+
+    path = request.scope.get("path", "") if request else ""
+    method = request.scope.get("method", "GET") if request else ""
+
+    key_str = f"{func.__module__}:{func.__name__}:{method}:{path}:{cache_kw}"
+    cache_key = hashlib.md5(key_str.encode()).hexdigest()
+    return f"{namespace}:{cache_key}"
+
+
+def user_key_builder(
+    func: Callable[..., Any],
+    namespace: str,
+    *,
+    request: Optional[Request] = None,
+    response: Optional[Any] = None,
+    args: Tuple[Any, ...],
+    kwargs: Dict[str, Any],
+) -> str:
+    """
+    Формирует уникальный ключ кэша для пользователя по его ID или токену.
+    Игнорирует AsyncSession.
+    """
+
+    exclude_types = (
+        AsyncSession,
+        SQLAlchemyUserDatabase,
+    )
+    filtered_kwargs = {
+        k: v for k, v in kwargs.items() if not isinstance(v, exclude_types)
+    }
+
+    user = filtered_kwargs.get("user")
+    user_id = getattr(user, "id", None)
+
+    if user_id is not None:
+        key_suffix = f"user:{user_id}"
+    else:
+        key_suffix = "anonymous"
+
+    key_str = f"{func.__module__}:{func.__name__}:{key_suffix}"
     cache_key = hashlib.md5(key_str.encode()).hexdigest()
 
     return f"{namespace}:{cache_key}"
