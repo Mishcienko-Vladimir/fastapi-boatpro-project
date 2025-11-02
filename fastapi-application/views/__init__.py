ef61257103103e30687ce3161d8869ab9cc5fdf5
Владимир <mishchienko.2001@mail.ru>
2025-11-02 21:39:32 +0300

    Init limit_exceeded view

--- fastapi-application/views/__init__.py
+++ fastapi-application/views/__init__.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from .admin import router as admin_router
 from .home import router as home_router
 from .page_missing import router as page_missing_router
+from .limit_exceeded import router as limit_exceeded_router
 from .favorites import router as favorites_router
 from .search import router as search_router
 from .products import router as catalog_router
@@ -17,6 +18,7 @@ router = APIRouter()
 router.include_router(admin_router)
 router.include_router(home_router)
 router.include_router(page_missing_router)
+router.include_router(limit_exceeded_router)
 router.include_router(favorites_router)
 router.include_router(search_router)
 router.include_router(catalog_router)
