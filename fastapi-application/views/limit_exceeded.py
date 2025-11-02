dac4d29747840ba20bbb176f80cb96d86d099562
Владимир <mishchienko.2001@mail.ru>
2025-11-02 21:39:24 +0300

    Create limit_exceeded view

---
+++ fastapi-application/views/limit_exceeded.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+from fastapi import APIRouter, Request, Depends
+
+from core.repositories.authentication.fastapi_users import optional_user
+from core.config import settings
+from core.models import User
+
+from utils.templates import templates
+
+
+router = APIRouter(
+    prefix=settings.view.limit_exceeded,
+)
+
+
+@router.get(
+    "/",
+    name="limit_exceeded",
+    include_in_schema=False,
+    response_model=None,
+)
+def limit_exceeded(
+    request: Request,
+    user: Optional[User] = Depends(optional_user),
+):
+    return templates.TemplateResponse(
+        name="limit-exceeded.html",
+        context={
+            "request": request,
+            "user": user,
+        },
+    )
