e229be9a02caaddabc29684655433aa5109c091f
Владимир <mishchienko.2001@mail.ru>
2025-11-04 20:08:26 +0300

    Refactoring default-time

--- fastapi-application/utils/limiter.py
+++ fastapi-application/utils/limiter.py
@@ -3,4 +3,4 @@ from slowapi.util import get_remote_address
 
 
 # Защита от спама (bruteforce).
-limiter = Limiter(key_func=get_remote_address, default_limits=["2/minute"])
+limiter = Limiter(key_func=get_remote_address, default_limits=["100/minute"])
