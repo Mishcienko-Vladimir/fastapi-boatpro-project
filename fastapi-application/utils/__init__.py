95cab62e4724f61db4bde9ad835e7c55da36d12c
Владимир <mishchienko.2001@mail.ru>
2025-11-04 20:05:30 +0300

    Init limiter

--- fastapi-application/utils/__init__.py
+++ fastapi-application/utils/__init__.py
@@ -1,7 +1,9 @@
 __all__ = (
     "camel_case_to_snake_case",
+    "limiter",
     "templates",
 )
 
 from .case_converter import camel_case_to_snake_case
+from .limiter import limiter
 from .templates import templates
