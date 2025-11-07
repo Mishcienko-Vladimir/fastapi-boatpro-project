55e386abd8764754685572e6942682c7db64c178
Владимир <mishchienko.2001@mail.ru>
2025-11-07 18:39:38 +0300

    Create tests for superuser

---
+++ fastapi-application/tests/test_utils/test_superuser.py
@@ -0,0 +1,44 @@
+import pytest
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from actions.create_superuser import create_superuser_if_not_exists
+
+from core.config import settings
+from core.models import User
+
+
+@pytest.mark.anyio
+async def test_create_superuser_if_not_exists_creates_user(test_session: AsyncSession):
+    """
+    Тест создания суперпользователя.
+    """
+
+    # Проверка, что суперпользователя нет.
+    result = await test_session.execute(
+        select(User).where(User.email == settings.admin.admin_email)
+    )
+    user = result.scalar_one_or_none()
+    assert user is None
+
+    # Создания суперпользователя.
+    user = await create_superuser_if_not_exists(test_session)
+    assert user is not None
+    assert user.email == settings.admin.admin_email
+    assert user.is_superuser is True
+
+
+@pytest.mark.anyio
+async def test_create_superuser_if_not_exists_does_not_duplicate(
+    test_session: AsyncSession,
+):
+    """
+    Тест, что суперпользователь не создаётся повторно.
+    """
+
+    user1 = await create_superuser_if_not_exists(test_session)
+    assert user1 is not None
+
+    user2 = await create_superuser_if_not_exists(test_session)
+    assert user2.id == user1.id
