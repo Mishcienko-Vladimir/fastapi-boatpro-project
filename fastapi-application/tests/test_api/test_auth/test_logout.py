8ddb5b15f6e8d8ef6cacebaea0be77be544ef80d
Владимир <mishchienko.2001@mail.ru>
2025-11-09 16:25:34 +0300

    Create test for logout

---
+++ fastapi-application/tests/test_api/test_auth/test_logout.py
@@ -0,0 +1,11 @@
+import pytest
+
+from httpx import AsyncClient
+
+
+@pytest.mark.anyio
+async def test_logout_success(logged_in_client: AsyncClient, prefix_auth: str):
+    """Успешный выход."""
+    response = await logged_in_client.post(url=f"{prefix_auth}/logout")
+    assert response.status_code == 204
+    assert "fastapiusersauth" not in response.cookies
