1ba10ac71c437fd6c68127a71eacd48ef746963a
Владимир <mishchienko.2001@mail.ru>
2025-11-09 19:38:57 +0300

    Create test for users list

---
+++ fastapi-application/tests/test_api/test_auth_users/test_users_list.py
@@ -0,0 +1,25 @@
+import pytest
+
+from httpx import AsyncClient
+
+
+@pytest.mark.anyio
+async def test_get_users_list_unauthorized(
+    client: AsyncClient,
+    prefix_users: str,
+):
+    """Неавторизованный пользователь может получить список."""
+    response = await client.get(prefix_users)
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+
+
+@pytest.mark.anyio
+async def test_get_users_list_authorized(
+    logged_in_client: AsyncClient,
+    prefix_users: str,
+):
+    """Авторизованный пользователь может получить список."""
+    response = await logged_in_client.get(prefix_users)
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
