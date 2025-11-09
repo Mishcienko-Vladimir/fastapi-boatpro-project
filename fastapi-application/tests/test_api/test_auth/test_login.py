b02b7b33fbd47591b13ed1a4738535c40fa10dd1
Владимир <mishchienko.2001@mail.ru>
2025-11-09 16:16:00 +0300

    Create tests for login

---
+++ fastapi-application/tests/test_api/test_auth/test_login.py
@@ -0,0 +1,59 @@
+import pytest
+
+from httpx import AsyncClient
+
+
+@pytest.mark.anyio
+async def test_login_success(
+    client: AsyncClient,
+    registered_user: dict,
+    prefix_auth: str,
+):
+    """Успешный вход."""
+    response = await client.post(
+        url=f"{prefix_auth}/login",
+        data={
+            "username": registered_user["email"],
+            "password": registered_user["password"],
+        },
+        headers={"Content-Type": "application/x-www-form-urlencoded"},
+    )
+    assert response.status_code == 204
+    assert "fastapiusersauth" in response.cookies
+
+
+@pytest.mark.anyio
+async def test_login_invalid_credentials(
+    client: AsyncClient,
+    registered_user: dict,
+    prefix_auth: str,
+):
+    """Вход с неверным паролем."""
+    response = await client.post(
+        url=f"{prefix_auth}/login",
+        data={
+            "username": registered_user["email"],
+            "password": "wrong-password",
+        },
+        headers={"Content-Type": "application/x-www-form-urlencoded"},
+    )
+    assert response.status_code == 400
+    assert response.json()["detail"] == "LOGIN_BAD_CREDENTIALS"
+
+
+@pytest.mark.anyio
+async def test_login_nonexistent_user(
+    client: AsyncClient,
+    prefix_auth: str,
+):
+    """Вход с несуществующим пользователем."""
+    response = await client.post(
+        url=f"{prefix_auth}/login",
+        data={
+            "username": "notexists@example.com",
+            "password": "password123",
+        },
+        headers={"Content-Type": "application/x-www-form-urlencoded"},
+    )
+    assert response.status_code == 400
+    assert response.json()["detail"] == "LOGIN_BAD_CREDENTIALS"
