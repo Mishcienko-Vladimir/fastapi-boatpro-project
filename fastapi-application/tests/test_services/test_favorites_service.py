b4b01956a0b6adc25e064ba48432f4a0138e6a90
Владимир <mishchienko.2001@mail.ru>
2025-11-16 22:34:18 +0300

    Refactoring test favorites services

--- fastapi-application/tests/test_services/test_favorites_service.py
+++ fastapi-application/tests/test_services/test_favorites_service.py
@@ -17,14 +17,14 @@ async def test_create_favorite(
     test_product: Product,
 ):
     """
-    Тест добавление в избранное.
+    Тест добавление в избранное, через сервис.
     """
-    service = FavoritesService(test_session)
+    service = FavoritesService(session=test_session)
     favorite_data = FavoriteCreate(
         user_id=test_user.id,
         product_id=test_product.id,
     )
-    favorite = await service.create_favorite(favorite_data)
+    favorite = await service.create_favorite(favorite_data=favorite_data)
 
     assert favorite.user_id == test_user.id
     assert favorite.product_id == test_product.id
@@ -46,16 +46,16 @@ async def test_get_favorites(
     test_product: Product,
 ):
     """
-    Тест получение избранных товаров.
+    Тест получение избранных товаров, через сервис.
     """
-    service = FavoritesService(test_session)
+    service = FavoritesService(session=test_session)
     favorite_data = FavoriteCreate(
         user_id=test_user.id,
         product_id=test_product.id,
     )
-    await service.create_favorite(favorite_data)
+    await service.create_favorite(favorite_data=favorite_data)
 
-    result = await service.get_favorites(test_user.id)
+    result = await service.get_favorites(user_id=test_user.id)
 
     assert len(result.favorites) >= 1
     assert result.favorites[0].product_id == test_product.id
@@ -68,16 +68,16 @@ async def test_delete_favorite(
     test_product: Product,
 ):
     """
-    Тест удаление из избранного.
+    Тест удаление из избранного, через сервис.
     """
-    service = FavoritesService(test_session)
+    service = FavoritesService(session=test_session)
     favorite_data = FavoriteCreate(
         user_id=test_user.id,
         product_id=test_product.id,
     )
-    favorite = await service.create_favorite(favorite_data)
+    favorite = await service.create_favorite(favorite_data=favorite_data)
 
-    await service.delete_favorite_by_id(favorite.id)
+    await service.delete_favorite_by_id(favorite_id=favorite.id)
 
     stmt = select(Favorite).where(Favorite.id == favorite.id)
     result = await test_session.execute(stmt)
