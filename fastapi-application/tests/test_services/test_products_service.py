9cb2f0f08ed8e053adddd9b8930bad6acfe638e7
Владимир <mishchienko.2001@mail.ru>
2025-11-16 22:34:28 +0300

    Refactoring test products services

--- fastapi-application/tests/test_services/test_products_service.py
+++ fastapi-application/tests/test_services/test_products_service.py
@@ -19,20 +19,28 @@ faker = Faker()
 @pytest.mark.anyio
 async def test_create_product_success(
     test_session: AsyncSession,
+    fake_boat_data: dict[str, Any],
     test_category: Category,
     mock_upload_file: UploadFile,
-    fake_boat_data: dict[str, Any],
 ):
-    """Тест создание товара."""
+    """
+    Тест создание товара, через сервис.
+    """
+    fake_boat_data["name"] = "Test Boat"
+    fake_boat_data["price"] = 50000
     create_data = BoatCreate(
-        name="Test Boat",
-        price=50000,
         category_id=test_category.id,
         **fake_boat_data,
     )
 
-    service = ProductsService(test_session, Boat)
-    product = await service.create_product(create_data, [mock_upload_file])
+    service = ProductsService(
+        session=test_session,
+        product_db=Boat,
+    )
+    product = await service.create_product(
+        product_data=create_data,
+        images=[mock_upload_file],
+    )
 
     assert product.id is not None
     assert product.name == "Test Boat"
@@ -49,15 +57,15 @@ async def test_create_product_success(
 @pytest.mark.anyio
 async def test_update_product_data_by_id(
     test_session: AsyncSession,
-    test_category: Category,
     fake_boat_data: dict[str, Any],
+    test_category: Category,
 ):
     """
-    Тест обновление данных товара.
+    Тест обновление данных товара, через сервис.
     """
+    fake_boat_data["name"] = "Old Name"
+    fake_boat_data["price"] = 10000
     boat = Boat(
-        name="Old Name",
-        price=10000,
         category_id=test_category.id,
         **fake_boat_data,
     )
@@ -65,12 +73,15 @@ async def test_update_product_data_by_id(
     await test_session.commit()
     await test_session.refresh(boat)
 
-    service = ProductsService(test_session, Boat)
+    service = ProductsService(
+        session=test_session,
+        product_db=Boat,
+    )
     new_boat_data = {"name": "Updated Boat", "price": 99999}
 
     updated = await service.update_product_data_by_id(
-        boat.id,
-        BoatUpdate(**new_boat_data),
+        product_id=boat.id,
+        product_data=BoatUpdate(**new_boat_data),
     )
 
     assert updated.name == "Updated Boat"
@@ -86,16 +97,16 @@ async def test_update_product_data_by_id(
 @pytest.mark.anyio
 async def test_delete_product_by_id(
     test_session: AsyncSession,
+    fake_boat_data: dict[str, Any],
     test_category: Category,
     mock_upload_file: UploadFile,
-    fake_boat_data: dict[str, Any],
 ):
     """
-    Тест удаление товара.
+    Тест удаление товара, через сервис.
     """
+    fake_boat_data["name"] = "Old Name"
+    fake_boat_data["price"] = 10000
     boat = Boat(
-        name="Old Name",
-        price=10000,
         category_id=test_category.id,
         **fake_boat_data,
     )
@@ -103,14 +114,17 @@ async def test_delete_product_by_id(
     await test_session.commit()
     await test_session.refresh(boat)
 
-    service = ProductsService(test_session, Boat)
+    service = ProductsService(
+        session=test_session,
+        product_db=Boat,
+    )
     updated_boat = await service.update_product_images_by_id(
         product_id=boat.id,
         remove_images=None,
         add_images=[mock_upload_file],
     )
 
-    await service.delete_product_by_id(updated_boat.id)
+    await service.delete_product_by_id(product_id=updated_boat.id)
 
     stmt = select(Boat).where(Boat.id == updated_boat.id)
     result = await test_session.execute(stmt)
