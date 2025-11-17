60ad325daa39aa3bed08c78091b5e1a86b1bfab9
Владимир <mishchienko.2001@mail.ru>
2025-11-17 14:43:04 +0300

    Removing excess

--- fastapi-application/tests/test_api/test_auth_users/conftest.py
+++ fastapi-application/tests/test_api/test_auth_users/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 
+from typing import Any
 from httpx import AsyncClient
-from sqlalchemy.ext.asyncio import AsyncSession
 from faker import Faker
 
 from core.config import settings
@@ -12,35 +12,29 @@ faker = Faker()
 
 
 @pytest.fixture(scope="module")
-def prefix_auth():
+def prefix_auth() -> str:
     """Префикс для всех auth-роутеров."""
     return f"{settings.api.prefix}{settings.api.v1.prefix}{settings.api.v1.auth}"
 
 
 @pytest.fixture(scope="module")
-def prefix_users():
+def prefix_users() -> str:
     """Префикс для users-роутеров."""
     return f"{settings.api.prefix}{settings.api.v1.prefix}{settings.api.v1.users}"
 
 
 @pytest.fixture(scope="function")
-def fake_user_data():
-    """Генерация тестовых данных пользователя."""
-    return {
-        "email": faker.email(),
-        "password": faker.password(),
-        "first_name": faker.first_name(),
-    }
-
-
-@pytest.fixture(scope="function")
 async def registered_user(
     client: AsyncClient,
-    test_session: AsyncSession,
     prefix_auth: str,
-    fake_user_data: dict,
-):
-    """Регистрирует пользователя и возвращает его данные."""
+    fake_user_data: dict[str, Any],
+) -> dict[str, Any]:
+    """
+    Регистрирует пользователя (через API) и возвращает его данные.
+    """
+    del fake_user_data["hashed_password"]
+    fake_user_data["password"] = faker.password()
+
     response = await client.post(
         url=f"{prefix_auth}/register",
         json=fake_user_data,
@@ -52,10 +46,12 @@ async def registered_user(
 @pytest.fixture(scope="function")
 async def logged_in_client(
     client: AsyncClient,
-    registered_user: dict,
+    registered_user: dict[str, Any],
     prefix_auth: str,
-):
-    """Возвращает клиент с активной сессией (кукой)."""
+) -> AsyncClient:
+    """
+    Возвращает клиент с активной сессией (кукой).
+    """
     response = await client.post(
         url=f"{prefix_auth}/login",
         data={
@@ -71,7 +67,9 @@ async def logged_in_client(
 
 @pytest.fixture(autouse=True)
 def reset_limiter():
-    """Сбрасывает счётчики лимитов перед каждым тестом."""
+    """
+    Сбрасывает счётчики лимитов перед каждым тестом.
+    """
     limiter.reset()
     yield
     limiter.reset()
