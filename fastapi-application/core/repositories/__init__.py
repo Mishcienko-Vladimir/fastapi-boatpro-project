bcb941b5f038056dc49dd8c55d18a6b026a3cc3e
Владимир <mishchienko.2001@mail.ru>
2025-11-19 19:09:52 +0300

    Create pickup-point-manager-crud

--- fastapi-application/core/repositories/__init__.py
+++ fastapi-application/core/repositories/__init__.py
@@ -1,7 +1,9 @@
 __all__ = (
     "UserManagerCrud",
     "FavoriteManagerCrud",
+    "PickupPointManagerCrud",
 )
 
 from .user_manager_crud import UserManagerCrud
 from .favorite_manager_crud import FavoriteManagerCrud
+from .pickup_point_manager_crud import PickupPointManagerCrud
