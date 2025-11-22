2349f37fc81e713b14d53f97b2278ca9b4bdfbaa
Владимир <mishchienko.2001@mail.ru>
2025-11-22 22:22:29 +0300

    Delete pickup-point-manager-crud

--- fastapi-application/core/repositories/__init__.py
+++ fastapi-application/core/repositories/__init__.py
@@ -1,9 +1,7 @@
 __all__ = (
     "ManagerCrud",
     "FavoriteManagerCrud",
-    "PickupPointManagerCrud",
 )
 
 from .manager_сrud import ManagerCrud
 from .favorite_manager_crud import FavoriteManagerCrud
-from .pickup_point_manager_crud import PickupPointManagerCrud
