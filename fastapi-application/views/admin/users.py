e71aa014b08fa98afcaa8d86319b3d551c8a04f9
Владимир <mishchienko.2001@mail.ru>
2025-11-22 21:37:40 +0300

    Refactoring admin-delete-user

--- fastapi-application/views/admin/users.py
+++ fastapi-application/views/admin/users.py
@@ -6,14 +6,17 @@ from fastapi import APIRouter, Request, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.api_v1.routers.users import get_users_list
-from core.models.user import SQLAlchemyUserDatabase
+from api.api_v1.dependencies.authentication.user_manager import get_user_manager
 
-from core.dependencies import get_db_session
-from core.repositories.authentication.fastapi_users import current_active_superuser
-from core.repositories.user_manager_crud import UserManagerCrud
 from core.config import settings
-from core.models import User
+from core.dependencies import get_db_session
 from core.schemas.user import UserRead
+
+from core.models.user import SQLAlchemyUserDatabase
+from core.models import User
+
+from core.repositories.authentication.user_manager import UserManager
+from core.repositories.authentication.fastapi_users import current_active_superuser
 
 from utils.templates import templates
 
@@ -75,23 +78,27 @@ async def admin_users(
 async def admin_delete_user(
     request: Request,
     session: Annotated[AsyncSession, Depends(get_db_session)],
-    user: Annotated[
-        User,
-        Depends(current_active_superuser),
-    ],
+    user_manager: Annotated[UserManager, Depends(get_user_manager)],
+    user: Annotated[User, Depends(current_active_superuser)],
     user_id_del: int = Form(...),
 ):
-    user_manager = UserManagerCrud(session=session)
-    user_delete = await user_manager.delete_user(user_id_del)
-    if not user_delete:
-        message = f"Пользователь с ID: {user_id_del} не найден."
-    else:
-        message = f"Пользователь с ID {user_id_del} успешно удалён."
+    try:
+        user_to_delete = await user_manager.user_db.get(user_id_del)
+        if not user_to_delete:
+            message = f"Пользователь с ID {user_id_del} не найден."
+        else:
+            await user_manager.delete(user_to_delete, request=request)
+            message = f"Пользователь с ID {user_id_del} успешно удалён."
 
-    users = await user_manager.get_all_users()
-    users_list = [UserRead.model_validate(u) for u in users]
-    number_verified = sum(1 for u in users_list if u.is_verified)
-    number_superuser = sum(1 for u in users_list if u.is_superuser)
+    except Exception as e:
+        message = f"Ошибка при удалении: {str(e)}"
+
+    # Обновляем список
+    users_db = SQLAlchemyUserDatabase(session, User)
+    users = await users_db.get_users()
+    users_list = [UserRead.model_validate(u).model_dump() for u in users]
+    number_verified = sum(1 for u in users_list if u["is_verified"])
+    number_superuser = sum(1 for u in users_list if u["is_superuser"])
 
     return templates.TemplateResponse(
         request=request,
