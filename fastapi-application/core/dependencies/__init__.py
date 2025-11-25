93f43bc847502e8ab95fd1852390b8a25426d072
Владимир <mishchienko.2001@mail.ru>
2025-11-25 13:31:57 +0300

    Moved fastapi-users in core/dependencies

--- fastapi-application/core/dependencies/__init__.py
+++ fastapi-application/core/dependencies/__init__.py
@@ -1,3 +1,13 @@
-__all__ = ("get_db_session",)
+__all__ = (
+    "get_db_session",
+    "optional_user",
+    "current_active_user",
+    "current_active_superuser",
+)
 
 from .get_db_session import get_db_session
+from .fastapi_users import (
+    optional_user,
+    current_active_user,
+    current_active_superuser,
+)
