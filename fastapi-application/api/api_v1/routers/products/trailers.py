12143f4d91769901560d6f9f3afc506bd4b425a3
Владимир <mishchienko.2001@mail.ru>
2025-11-26 18:41:01 +0300

    Add doc for router trailers

--- fastapi-application/api/api_v1/routers/products/trailers.py
+++ fastapi-application/api/api_v1/routers/products/trailers.py
@@ -1,10 +1,9 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends, UploadFile, Form, File
+from fastapi import APIRouter, Depends, UploadFile, Form, File, status
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from fastapi_cache import FastAPICache
 from fastapi_cache.decorator import cache
-
-from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.api_v1.dependencies.create_multipart_form_data import (
     create_multipart_form_data,
@@ -28,10 +27,25 @@ from utils.key_builder import (
 )
 
 
-router = APIRouter(prefix=settings.api.v1.trailers, tags=["Прицепы 🚛"])
+router = APIRouter(
+    prefix=settings.api.v1.trailers,
+    tags=["Прицепы 🚛"],
+)
 
 
-@router.post("/", status_code=201, response_model=TrailerRead)
+@router.post(
+    path="/",
+    response_model=TrailerRead,
+    status_code=status.HTTP_201_CREATED,
+    operation_id="create_trailer",
+    summary="Создание нового прицепа",
+    responses={
+        201: {"model": TrailerRead},
+        400: {"description": "Имя уже занято или данные некорректны."},
+        422: {"description": "Ошибка валидации входных данных."},
+        500: {"description": "Внутренняя ошибка сервера."},
+    },
+)
 async def create_trailer(
     session: Annotated[
         AsyncSession,
@@ -47,19 +61,56 @@ async def create_trailer(
     ],
 ) -> TrailerRead:
     """
-    Создание нового прицепа.
+    ## Создание нового прицепа.
+
+    **Описание:**
+    Используется для создания нового прицепа в админ-панели.
+
+    **Принимает поля:**
+    - `category_id`: ID категории товара (int, id > 0).
+    - `name`: Название модели (str, 1–255 символов, уникальное).
+    - `price`: Цена в рублях (int, цена > 0).
+    - `company_name`: Название производителя (str, 1–100 символов).
+    - `description`: Описание товара (str, может быть пустым).
+    - `is_active`: Наличие товара (bool).
+    - `full_mass`: Полный вес прицепа в кг (int, 32767 > вес > 0).
+    - `load_capacity`: Грузоподъемность в кг (int, 32767 > грузоподъемность > 0).
+    - `trailer_length`: Длина прицепа в см (int, 32767 > длина > 0).
+    - `max_ship_length`: Максимальная длина перевозимого судна в см (int, 32767 > длина > 0).
+    - `images`: Список изображений товара (множественная загрузка, формат: image/*).
+
+    **Ответы:**
+    - `201 Created` — прицеп успешно создан. Возвращает созданный прицеп.
+    - `400 Bad Request` — имя занято или данные некорректны.
+    - `422 Unprocessable Entity` — ошибка валидации входных данных.
+    - `500 Internal Server Error` — внутренняя ошибка сервера.
     """
-    _service = ProductsService(session, Trailer)
-    new_trailer = await _service.create_product(trailer_data, images)
+    _service = ProductsService(session=session, product_db=Trailer)
+    new_trailer = await _service.create_product(
+        product_data=trailer_data,
+        images=images,
+    )
     await FastAPICache.clear(namespace=settings.cache.namespace.trailers_list)
     await FastAPICache.clear(namespace=settings.cache.namespace.trailer)
     return TrailerRead.model_validate(new_trailer)
 
 
-@router.get("/trailer-name/{trailer_name}", status_code=200, response_model=TrailerRead)
+@router.get(
+    path="/trailer-name/{trailer_name}",
+    response_model=TrailerRead,
+    status_code=status.HTTP_200_OK,
+    operation_id="get_trailer_by_name",
+    summary="Получение прицепа по названию",
+    responses={
+        200: {"model": TrailerRead},
+        404: {"description": "Прицеп не найден."},
+        422: {"description": "Некорректный формат названия прицепа"},
+        500: {"description": "Внутренняя ошибка сервера."},
+    },
+)
 @cache(
     expire=300,
-    key_builder=get_by_name_key_builder,
+    key_builder=get_by_name_key_builder,  # type: ignore
     namespace=settings.cache.namespace.trailer,
 )
 async def get_trailer_by_name(
@@ -67,17 +118,41 @@ async def get_trailer_by_name(
     trailer_name: str,
 ) -> TrailerRead:
     """
-    Получение прицепа по названию.
+    ## Получение прицепа по названию.
+
+    **Описание:**
+    Используется для получения прицепа по его названию в админ-панели и на сайте.
+
+    **Принимает поле:**
+    - `trailer_name`: Название прицепа (str, 1–255 символов).
+
+    **Ответы:**
+    - `200 OK` — прицеп успешно найден. Возвращает объект прицепа.
+    - `404 Not Found` — прицеп не найден.
+    - `422 Unprocessable Entity` — некорректный формат имени.
+    - `500 Internal Server Error` — внутренняя ошибка сервера.
     """
-    _service = ProductsService(session, Trailer)
-    trailer = await _service.get_product_by_name(trailer_name)
+    _service = ProductsService(session=session, product_db=Trailer)
+    trailer = await _service.get_product_by_name(product_name=trailer_name)
     return TrailerRead.model_validate(trailer)
 
 
-@router.get("/trailer-id/{trailer_id}", status_code=200, response_model=TrailerRead)
+@router.get(
+    path="/trailer-id/{trailer_id}",
+    response_model=TrailerRead,
+    status_code=status.HTTP_200_OK,
+    operation_id="get_trailer_by_id",
+    summary="Получение прицепа по id",
+    responses={
+        200: {"model": TrailerRead},
+        404: {"description": "Прицеп не найден."},
+        422: {"description": "Некорректный формат id"},
+        500: {"description": "Внутренняя ошибка сервера."},
+    },
+)
 @cache(
     expire=300,
-    key_builder=get_by_id_key_builder,
+    key_builder=get_by_id_key_builder,  # type: ignore
     namespace=settings.cache.namespace.trailer,
 )
 async def get_trailer_by_id(
@@ -85,45 +160,94 @@ async def get_trailer_by_id(
     trailer_id: int,
 ) -> TrailerRead:
     """
-    Получение прицепа по id.
+    ## Получение прицепа по id.
+
+    **Описание:**
+    Используется для получения прицепа по его ID в админ-панели и на сайте.
+
+    **Принимает поле:**
+    - `trailer_id`: ID прицепа (int, целое число).
+
+    **Ответы:**
+    - `200 OK` — прицеп успешно найден. Возвращает объект прицепа.
+    - `404 Not Found` — прицеп не найден.
+    - `422 Unprocessable Entity` — некорректный формат ID.
+    - `500 Internal Server Error` — внутренняя ошибка сервера.
     """
-    _service = ProductsService(session, Trailer)
-    trailer = await _service.get_product_by_id(trailer_id)
+    _service = ProductsService(session=session, product_db=Trailer)
+    trailer = await _service.get_product_by_id(product_id=trailer_id)
     return TrailerRead.model_validate(trailer)
 
 
-@router.get("/", status_code=200, response_model=list[TrailerRead])
+@router.get(
+    path="/",
+    response_model=list[TrailerRead],
+    status_code=status.HTTP_200_OK,
+    operation_id="get_trailers",
+    summary="Получение всех прицепов",
+    responses={
+        200: {"model": list[TrailerRead]},
+        404: {"description": "Список пуст."},
+        500: {"description": "Внутренняя ошибка сервера."},
+    },
+)
 @cache(
     expire=300,
-    key_builder=universal_list_key_builder,
+    key_builder=universal_list_key_builder,  # type: ignore
     namespace=settings.cache.namespace.trailers_list,
 )
 async def get_trailers(
     session: Annotated[AsyncSession, Depends(get_db_session)],
 ) -> list[TrailerRead]:
     """
-    Получение всех прицепов.
+    ## Получение всех прицепов.
+
+    **Описание:**
+    Используется для получения всех прицепов в админ-панели и на сайте.
+
+    **Ответы:**
+    - `200 OK` — список найден. Возвращает список прицепов.
+    - `404 Not Found` — список пуст.
+    - `500 Internal Server Error` — внутренняя ошибка сервера.
     """
-    _service = ProductsService(session, Trailer)
+    _service = ProductsService(session=session, product_db=Trailer)
     all_trailers = await _service.get_products()
     return [TrailerRead.model_validate(trailer) for trailer in all_trailers]
 
 
-@router.get("/summary", status_code=200, response_model=list[TrailerSummarySchema])
+@router.get(
+    path="/summary",
+    response_model=list[TrailerSummarySchema],
+    status_code=status.HTTP_200_OK,
+    operation_id="get_trailers_summary",
+    summary="Получение краткой информации о всех прицепах",
+    responses={
+        200: {"model": list[TrailerSummarySchema]},
+        404: {"description": "Список пуст."},
+        500: {"description": "Внутренняя ошибка сервера."},
+    },
+)
 @cache(
     expire=300,
-    key_builder=universal_list_key_builder,
+    key_builder=universal_list_key_builder,  # type: ignore
     namespace=settings.cache.namespace.trailers_list,
 )
 async def get_trailers_summary(
     session: Annotated[AsyncSession, Depends(get_db_session)],
 ) -> list[TrailerSummarySchema]:
     """
-    Получает краткую информацию о всех прицепах.
-    В данных только одно изображение для каждого прицепа.
-    """
+    ## Получение краткой информации о всех прицепах.
 
-    _service = ProductsService(session, Trailer)
+    **Описание:**
+    Используется для отображения списка прицепов на главной или в каталоге.
+    В данных только одно изображение для каждого прицепа.
+
+    **Ответы:**
+    - `200 OK` — список найден. Возвращает краткие объекты прицепов.
+    - `404 Not Found` — список пуст.
+    - `500 Internal Server Error` — внутренняя ошибка сервера.
+    """
+    _service = ProductsService(session=session, product_db=Trailer)
     all_trailers = await _service.get_products()
     return [
         TrailerSummarySchema.model_validate(
@@ -136,27 +260,73 @@ async def get_trailers_summary(
     ]
 
 
-@router.patch("/{trailer_id}", status_code=200, response_model=TrailerRead)
+@router.patch(
+    path="/{trailer_id}",
+    response_model=TrailerRead,
+    status_code=status.HTTP_200_OK,
+    operation_id="update_trailer_data_by_id",
+    summary="Частичное обновление данных прицепа по id",
+    responses={
+        200: {"model": TrailerRead},
+        400: {"description": "Имя уже занято или данные некорректны."},
+        404: {"description": "Прицеп с таким id не найден."},
+        422: {"description": "Ошибка валидации входных данных."},
+        500: {"description": "Внутренняя ошибка сервера."},
+    },
+)
 async def update_trailer_data_by_id(
     session: Annotated[AsyncSession, Depends(get_db_session)],
     trailer_id: int,
     trailer_data: TrailerUpdate,
 ) -> TrailerRead:
     """
-    Обновление данных прицепа по id (кроме изображений).
+    ## Частичное обновление данных прицепа, кроме изображений.
+
+    **Описание:**
+    Используется для частичного обновления данных прицепа в админ-панели.
+
+    **Принимает поля:**
+    - `trailer_id`: ID прицепа (int, целое число), для изменения.
+    - `name`: Название модели (str, 1–255 символов, уникальное). Если не указано, не изменяется.
+    - `price`: Цена в рублях (int, цена > 0). Если не указано, не изменяется.
+    - `company_name`: Название производителя (str, 1–100 символов). Если не указано, не изменяется.
+    - `description`: Описание товара (str, может быть пустым). Если не указано, не изменяется.
+    - `is_active`: Наличие товара (bool). Если не указано, не изменяется.
+    - `full_mass`: Полный вес прицепа в кг (int, 32767 > вес > 0). Если не указано, не изменяется.
+    - `load_capacity`: Грузоподъемность в кг (int, 32767 > грузоподъемность > 0). Если не указано, не изменяется.
+    - `trailer_length`: Длина прицепа в см (int, 32767 > длина > 0). Если не указано, не изменяется.
+    - `max_ship_length`: Максимальная длина перевозимого судна в см (int, 32767 > длина > 0). Если не указано, не изменяется.
+
+    **Ответы:**
+    - `200 OK` — прицеп успешно обновлён. Возвращает обновлённый прицеп.
+    - `400 Bad Request` — имя занято или данные некорректны.
+    - `404 Not Found` — прицеп не найден.
+    - `422 Unprocessable Entity` — ошибка валидации.
+    - `500 Internal Server Error` — внутренняя ошибка сервера.
     """
-    _service = ProductsService(session, Trailer)
-    trailer = await _service.update_product_data_by_id(trailer_id, trailer_data)
-    await FastAPICache.clear(
-        namespace=settings.cache.namespace.trailers_list,
+    _service = ProductsService(session=session, product_db=Trailer)
+    trailer = await _service.update_product_data_by_id(
+        product_id=trailer_id,
+        product_data=trailer_data,
     )
-    await FastAPICache.clear(
-        namespace=settings.cache.namespace.trailer,
-    )
+    await FastAPICache.clear(namespace=settings.cache.namespace.trailers_list)
+    await FastAPICache.clear(namespace=settings.cache.namespace.trailer)
     return TrailerRead.model_validate(trailer)
 
 
-@router.patch("/images/{trailer_id}", status_code=200, response_model=TrailerRead)
+@router.patch(
+    path="/images/{trailer_id}",
+    response_model=TrailerRead,
+    status_code=status.HTTP_200_OK,
+    operation_id="update_trailer_images_by_id",
+    summary="Обновление изображений прицепа по id",
+    responses={
+        200: {"model": TrailerRead},
+        404: {"description": "Прицеп или изображения не найдены."},
+        422: {"description": "Некорректный формат ID изображений."},
+        500: {"description": "Внутренняя ошибка сервера."},
+    },
+)
 async def update_trailer_images_by_id(
     session: Annotated[AsyncSession, Depends(get_db_session)],
     trailer_id: int,
@@ -170,37 +340,67 @@ async def update_trailer_images_by_id(
     ),
 ) -> TrailerRead:
     """
-    Обновление изображений прицепа по id.
+    ## Обновление изображений прицепа.
+
+    **Описание:**
+    Используется для обновления изображений прицепа в админ-панели.
+
+    **Принимает поля:**
+    - `trailer_id`: ID прицепа (int, целое число), у которого изменяются изображения.
+    - `remove_images`: Строка с ID изображений (через запятую, без пробелов), которые нужно удалить (может быть пустой).
+    - `add_images`: Список новых изображений (множественная загрузка (зажимая shift и выбирая фото), формат: image/*).
+
+    **Ответы:**
+    - `200 OK` — успешно обновлено. Возвращает обновлённый прицеп.
+    - `404 Not Found` — прицеп или изображения не найдены.
+    - `422 Unprocessable Entity` — некорректный формат ID изображений.
+    - `500 Internal Server Error` — внутренняя ошибка сервера (например, файл не найден).
     """
-    _service = ProductsService(session, Trailer)
+    _service = ProductsService(session=session, product_db=Trailer)
     trailer = await _service.update_product_images_by_id(
-        trailer_id,
-        remove_images,
-        add_images,
+        product_id=trailer_id,
+        remove_images=remove_images,
+        add_images=add_images,
     )
-    await FastAPICache.clear(
-        namespace=settings.cache.namespace.trailers_list,
-    )
-    await FastAPICache.clear(
-        namespace=settings.cache.namespace.trailer,
-    )
+    await FastAPICache.clear(namespace=settings.cache.namespace.trailers_list)
+    await FastAPICache.clear(namespace=settings.cache.namespace.trailer)
     return TrailerRead.model_validate(trailer)
 
 
-@router.delete("/{trailer_id}", status_code=204)
+@router.delete(
+    path="/{trailer_id}",
+    response_model=None,
+    status_code=status.HTTP_204_NO_CONTENT,
+    operation_id="delete_trailer_by_id",
+    summary="Удаление прицепа по id",
+    responses={
+        204: {"description": "Прицеп успешно удалён, ответ пуст."},
+        404: {"description": "Прицеп не найден."},
+        422: {"description": "Некорректный формат ID."},
+        500: {"description": "Внутренняя ошибка сервера (например, файлы не найдены)."},
+    },
+)
 async def delete_trailer_by_id(
     session: Annotated[AsyncSession, Depends(get_db_session)],
     trailer_id: int,
 ) -> None:
     """
-    Удаление прицепа по id.
+    ## Удаление прицепа по id.
+
+    **Описание:**
+    Используется для удаления прицепа в админ-панели.
+
+    **Принимает поле:**
+    - `trailer_id`: ID прицепа (int, целое число), для удаления.
+
+    **Ответы:**
+    - `204 No Content` — прицеп успешно удалён. Ничего не возвращается.
+    - `404 Not Found` — прицеп не найден.
+    - `422 Unprocessable Entity` — некорректный ID.
+    - `500 Internal Server Error` — внутренняя ошибка сервера (например, файлы не найдены).
     """
-    _service = ProductsService(session, Trailer)
-    delete_trailer = await _service.delete_product_by_id(trailer_id)
-    await FastAPICache.clear(
-        namespace=settings.cache.namespace.trailers_list,
-    )
-    await FastAPICache.clear(
-        namespace=settings.cache.namespace.trailer,
-    )
+    _service = ProductsService(session=session, product_db=Trailer)
+    delete_trailer = await _service.delete_product_by_id(product_id=trailer_id)
+    await FastAPICache.clear(namespace=settings.cache.namespace.trailers_list)
+    await FastAPICache.clear(namespace=settings.cache.namespace.trailer)
     return delete_trailer
