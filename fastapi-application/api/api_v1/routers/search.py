d032073be0cd99c170716685e68bb68519a04ceb
Владимир <mishchienko.2001@mail.ru>
2025-11-26 18:40:17 +0300

    Add doc for router search

--- fastapi-application/api/api_v1/routers/search.py
+++ fastapi-application/api/api_v1/routers/search.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.api_v1.services.products.products_service import ProductsService
@@ -10,20 +10,45 @@ from core.models.products import Product
 from core.schemas.products import ProductBaseModelRead
 
 
-router = APIRouter(prefix=settings.api.v1.search, tags=["Поиск 🔍"])
+router = APIRouter(
+    prefix=settings.api.v1.search,
+    tags=["Поиск 🔍"],
+)
 
 
-@router.get("/", response_model=list[ProductBaseModelRead])
+@router.get(
+    path="/",
+    response_model=list[ProductBaseModelRead],
+    status_code=status.HTTP_200_OK,
+    operation_id="search_products",
+    summary="Поиск товаров по ключевому слову",
+    responses={
+        200: {"model": list[ProductBaseModelRead]},
+        500: {"description": "Внутренняя ошибка сервера"},
+    },
+)
 async def search_products(
     session: Annotated[AsyncSession, Depends(get_db_session)],
     query: str,
 ) -> list[ProductBaseModelRead]:
     """
-    Поиск товаров по ключевому слову (название, производитель, описание).
-    """
-    _service = ProductsService(session, Product)
-    products = await _service.get_search_products(query)
+    ## Поиск товаров по ключевому слову.
 
+    **Описание:**
+    Используется для поиска товаров по совпадению в полях:
+    - `название`
+    - `производитель`
+    - `описание`
+
+    **Принимает параметр:**
+    - `query`: Строка поиска (str, минимум 1 символ).
+
+    **Ответы:**
+    - `200 OK` — найдены товары. Возвращает список товаров с краткой информацией и одним изображением.
+    - `500 Internal Server Error` — внутренняя ошибка сервера.
+    """
+    _service = ProductsService(session=session, product_db=Product)
+    products = await _service.get_search_products(query=query)
     return [
         ProductBaseModelRead.model_validate(
             {
