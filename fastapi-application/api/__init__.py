86ea7c5a9044d7595464ef0655128697538a268f
Владимир <mishchienko.2001@mail.ru>
2025-11-28 14:20:23 +0300

    Init webhooks

--- fastapi-application/api/__init__.py
+++ fastapi-application/api/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
 from api.api_v1.routers import router as router_api_v1
+from api.webhooks import webhooks_router
 from core.config import settings
 
 
 router = APIRouter(prefix=settings.api.prefix)
 router.include_router(router_api_v1)
+router.include_router(webhooks_router)
